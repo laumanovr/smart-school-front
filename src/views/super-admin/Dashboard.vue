25372fe63566c970dc9f354276b564b86c036758
Abdykapar <abdykaparkamilov@gmail.com>
2020-07-30 23:07:00 +0600

    add super-admin dashboard page

--- src/views/super-admin/Dashboard.vue
+++ src/views/super-admin/Dashboard.vue
@@ -1,8 +1,30 @@
 <template>
 	<div class="dashboard">
 		<SuperAdminHeader></SuperAdminHeader>
-		<super-admin-sidebar />
-		<div class="dashboard__body"></div>
+		<div class="dashboard__container">
+			<super-admin-sidebar />
+			<div class="dashboard__body">
+				<div>
+					<img src="../../assets/images/temporary/students.svg" alt="">
+					<img src="../../assets/images/temporary/teachers-avg.svg" alt="">
+					<img src="../../assets/images/temporary/attendance.svg" alt="">
+					<img src="../../assets/images/temporary/homework.svg" alt="">
+				</div>
+				<div>
+					<div>
+						<div>
+							<img src="../../assets/images/temporary/teacher-active.svg" alt="">
+						</div>
+						<div>
+							<img src="../../assets/images/temporary/lesson-lost.svg" alt="">
+							<img src="../../assets/images/temporary/lesson-hours.svg" alt="">
+						</div>
+					</div>
+					<img src="../../assets/images/temporary/std-avg.svg" alt="">
+					<img src="../../assets/images/temporary/school-perf.svg" alt="">
+				</div>
+			</div>
+		</div>
 	</div>
 </template>
 
@@ -24,5 +46,45 @@ export default {
 	.dashboard {
 	  height: 100vh;
 	  background: #E5E5E5;
+
+	  &__container {
+	    display: flex;
+	  }
+	  &__body {
+	    margin: 20px 0;
+	    width: calc(100% - 96px);
+
+	    > div {
+	      display: flex;
+	      justify-content: space-evenly;
+	      margin: 20px 0;
+	    }
+	    > div:first-child {
+	      img {
+	        width: 20%;
+	      }
+	    }
+	    > div:last-child {
+	      > div {
+	        width: 44%;
+	        > div:first-child {
+              img {
+                width: 100%;
+              }
+	        }
+	        div:last-child {
+	          display: flex;
+	          justify-content: space-between;
+	          margin-top: 10px;
+	        }
+	        img {
+	          width: 48%;
+	        }
+	      }
+	      img {
+	        width: 20%;
+	      }
+	    }
+	  }
 	}
 </style>
