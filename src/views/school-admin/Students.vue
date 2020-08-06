a069a1611d82fb972d8f2b1ff67160837d8f5fe8
Ramazan <laumanov.r@gmail.com>
2020-08-06 12:11:40 +0600

    create students page and create student-class logic

---
+++ src/views/school-admin/Students.vue
@@ -0,0 +1,292 @@
+<template>
+    <div class="students-container">
+        <SuperAdminSchoolHead>
+            <template v-slot:title>Студенты</template>
+            <template v-slot:center>
+                <SmartButton @clicked="isAddStudentModal = true">
+                    Добавить ученика <img src="../../assets/images/icons/add-user.svg" alt="">
+                </SmartButton>
+                <SmartSearchInput></SmartSearchInput>
+            </template>
+            <template v-slot:right>
+                <SmartBtn2>
+                    Import <img style="padding-bottom: 5px" src="../../assets/images/icons/import.svg" alt="">
+                </SmartBtn2>
+                <SmartBtn2>
+                    Export <img src="../../assets/images/icons/export.svg" alt="">
+                </SmartBtn2>
+                <SmartBtn2>
+                    Download Template <img src="../../assets/images/icons/download.svg" alt="">
+                </SmartBtn2>
+            </template>
+        </SuperAdminSchoolHead>
+        <SmartTable :schools="students">
+            <template v-slot:firstItem>
+                <SmartSelect>Класс <v-icon>$chevronDown</v-icon></SmartSelect>
+                <SmartSelect>Буква <v-icon>$chevronDown</v-icon></SmartSelect>
+                <SmartSelect>Пол <v-icon>$chevronDown</v-icon></SmartSelect>
+            </template>
+            <template v-slot:head>
+                <th>Ф.И.О</th>
+                <th>Класс</th>
+                <!--<th>ID</th>-->
+                <th>Пол</th>
+                <th>Дата рождения</th>
+                <th>Имя Родителя</th>
+                <th>Логин</th>
+                <!--<th>Телефон Родителя</th>-->
+                <th></th>
+            </template>
+
+            <template v-slot:body="{ item }">
+                <td>{{ item.name }}</td>
+                <td>{{ item.classTitle }}</td>
+                <td>{{ item.gender ? 'Ж' : 'M' }}</td>
+                <td>{{ item.dateOfBirth }}</td>
+                <td></td>
+                <td></td>
+                <td><img src="../../assets/images/icons/pen.svg" alt=""></td>
+            </template>
+        </SmartTable>
+        <v-dialog v-model="isAddStudentModal" width="546" id="add-form">
+            <v-form @submit.prevent="submitStudent" ref="form">
+                <div class="form-head">
+                    <span>Добавить ученика</span>
+                    <img src="../../assets/images/profile-icon.svg" alt="">
+                    <button class="profile-edit">
+                        <img src="../../assets/images/icons/edit.svg">
+                    </button>
+                </div>
+
+                <div>
+                    <v-text-field v-model="studentObj.name" :rules="required" label="ФИО"></v-text-field>
+                </div>
+
+                <div>
+                    <v-radio-group v-model="studentObj.gender" :rules="required" :mandatory="false" row>
+                        <v-radio label="Male" value="MALE"></v-radio>
+                        <v-radio label="Female" value="FEMALE"></v-radio>
+                    </v-radio-group>
+                    <v-menu
+                        v-model="menu2"
+                        :close-on-content-click="false"
+                        :nudge-right="40"
+                        transition="scale-transition"
+                        offset-y
+                        min-width="290px"
+                    >
+                        <template v-slot:activator="{ on, attrs }">
+                            <v-text-field
+                                v-model="birthday"
+                                label="Birthday"
+                                readonly
+                                v-bind="attrs"
+                                v-on="on"
+                            ></v-text-field>
+                        </template>
+                        <v-date-picker v-model="birthday" @input="menu2 = false"></v-date-picker>
+                    </v-menu>
+                </div>
+
+                <div>
+                    <v-select
+                        :rules="required"
+                        :items="classes"
+                        item-text="classTitle"
+                        item-value="classId"
+                        label="Класс"
+                        v-model="studentObj.classId"
+                    >
+                    </v-select>
+                </div>
+
+                <div class="spacer">
+                    <v-text-field v-model="studentObj.phone" label="Номер телефона"></v-text-field>
+                </div>
+
+                <div>
+                    <v-text-field  label="Имя родителя"></v-text-field>
+                </div>
+
+                <div>
+                    <v-text-field  label="Телефон родителя"></v-text-field>
+                </div>
+
+                <div class="form-footer">
+                    <v-btn type="submit" color="primary">Сохранить</v-btn>
+                    <v-btn @click="isAddStudentModal=false">Отменить</v-btn>
+                </div>
+            </v-form>
+        </v-dialog>
+    </div>
+</template>
+
+<script>
+    import SuperAdminSchoolHead from '@/components/super-admin/schools/SuperAdminSchoolHead';
+    import SmartTable from '@/components/table/SmartTable';
+    import SmartButton from '@/components/button/SmartButton';
+    import SmartSearchInput from '@/components/input/SmartSearchInput';
+    import SmartBtn2 from '@/components/button/SmartBtn2';
+    import SmartSelect from '@/components/select/SmartSelect';
+    import { LanguageService } from '@/_services/language.service';
+    const languageService = new LanguageService();
+    import { InstructorCourseService } from '@/_services/instructor-course.service'
+    const instructorCourseService = new InstructorCourseService();
+    import {SchoolClassService} from '@/_services/school-class.service';
+    const schoolClassService = new SchoolClassService();
+    import {InstructorClassService} from '@/_services/instructor-class.service';
+    const instructorClassService = new InstructorClassService();
+    import {StudentService} from '@/_services/student.service';
+    const studentService = new StudentService();
+    import moment from 'moment';
+    import { RoleService } from '@/_services/role.service'
+    const roleService = new RoleService();
+    import {StudentClassService} from '@/_services/student-class.service';
+    const studentClassService = new StudentClassService();
+
+    export default {
+        components: {
+            SmartSelect,
+            SmartBtn2,
+            SmartSearchInput,
+            SmartButton,
+            SuperAdminSchoolHead,
+            SmartTable
+        },
+
+        data() {
+            return {
+                studentObj: {
+                    address: '',
+                    avatar: '',
+                    chronicleYearId: 1,
+                    classId: 0,
+                    comeBy: '',
+                    dateOfBirth: '',
+                    email: '',
+                    enabled: true,
+                    gender: '',
+                    languageId: '',
+                    leaveBy: '',
+                    middleName: '',
+                    name: '',
+                    nationality: '',
+                    password: '',
+                    phone: '',
+                    pin: '',
+                    schoolId: 0,
+                    surname: '',
+                    username: '',
+                    roles: [
+                        0
+                    ],
+                },
+                studentClassObj: {
+                    active: true,
+                    chronicleId: 1,
+                    classId: 0,
+                    studentId: 0
+                },
+                students: [],
+                classes: [],
+                required: [v => !!v || 'Input is required'],
+                isAddStudentModal: false,
+                roles: [],
+                languages: [],
+                birthday: '2000-2-11',
+                menu2: false,
+            }
+        },
+
+        computed: {
+            userProfile () {
+                return this.$store.state.account.profile
+            }
+        },
+
+        created() {
+            this.studentObj.languageId = this.userProfile.schools[0].languageId;
+            this.studentObj.chronicleYearId = this.userProfile.schools[0].chronicleId;
+            this.studentClassObj.chronicleId = this.userProfile.schools[0].chronicleId;
+            this.fetchAllClasses();
+            this.fetchRoles();
+            this.fetchStudents();
+        },
+
+        methods: {
+            fetchStudents() {
+                studentService.getAllBySchool(this.userProfile.schools[0].id).then((res) => {
+                    this.students = res;
+                })
+            },
+
+            fetchRoles () {
+                roleService.listPageable(0).then(res => {
+                    this.roles = res;
+                }).catch(err => console.log(err));
+            },
+
+            fetchAllClasses() {
+                instructorClassService.getAllClasses(this.userProfile.schools[0].id).then((res) => {
+                    this.classes = res
+                })
+            },
+
+            submitStudent() {
+                this.studentObj.schoolId = this.userProfile.schools[0].id;
+                this.studentObj.dateOfBirth = moment(this.birthday, 'YYYY-MM-DD').format('DD.MM.YYYY');
+                this.studentObj.roles = this.roles.filter(i => i.code === 'ROLE_STUDENT').map(i => i.id);
+                studentService.create(this.studentObj).then((res) => {
+                    let studentId = parseInt(res.message);
+                    this.studentClassObj.classId = this.studentObj.classId;
+                    this.studentClassObj.studentId = studentId;
+
+                    studentClassService.create(this.studentClassObj).then((res) => {
+                        this.fetchStudents();
+                        this.isAddStudentModal = false;
+                        this.$toast.success('Success message');
+                    })
+                })
+            }
+        }
+    }
+</script>
+
+<style lang="scss">
+    .v-form {
+        background: #FFFFFF;
+        border-radius: 7px;
+        padding: 20px 40px;
+
+        > div {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            position: relative;
+
+            .v-input--radio-group {
+                margin-right: 20px;
+            }
+
+            .profile-edit {
+                background: #FFFFFF;
+                box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.08);
+                width: 24px;
+                height: 24px;
+                border-radius: 50%;
+                position: absolute;
+                right: 0;
+                bottom: 0;
+
+                img {
+                    margin-top: 5px;
+                }
+            }
+            &.spacer {
+                .v-text-field:first-child {
+                    margin-right: 20px;
+                }
+            }
+        }
+    }
+</style>
