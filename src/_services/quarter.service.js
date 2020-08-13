44c7ff1a925df28a4bfcf11f2b489c3411941e21
Ramazan <laumanov.r@gmail.com>
2020-08-13 15:07:34 +0600

    create quarter-service

---
+++ src/_services/quarter.service.js
@@ -0,0 +1,43 @@
+import { BaseApiService } from '@/_services/baseApi.service';
+
+export class QuarterService extends BaseApiService {
+
+    constructor() {
+        super();
+    }
+
+    getListAll() {
+        const url = `${process.env.VUE_APP_BASE_URL}/v1/quarter/list`;
+        return this.sendGetRequest(url);
+    }
+
+    generateQuarter(school_id, chronicle_id){
+        const url = `${process.env.VUE_APP_BASE_URL}/v1/quarter/generate?chronicleId=${chronicle_id}&schoolId=${school_id}`;
+        return this.sendPostRequest(url, {});
+    }
+
+    getBySchoolAndChronicle(school_id, chronicle_id){
+        const url = `${process.env.VUE_APP_BASE_URL}/v1/quarter/list/school/${school_id}/chronicle/${chronicle_id}`;
+        return this.sendGetRequest(url);
+    }
+
+    getById(id) {
+        const url = `${process.env.VUE_APP_BASE_URL}/v1/quarter/${id}`;
+        return this.sendGetRequest(url);
+    }
+
+    create(data) {
+        const url = `${process.env.VUE_APP_BASE_URL}/v1/quarter/create`;
+        return this.sendPostRequest(url, data);
+    }
+
+    edit(data,quarter_id) {
+        const url = `${process.env.VUE_APP_BASE_URL}/v1/quarter/edit/${quarter_id}`;
+        return this.sendPostRequest(url, data);
+    }
+
+    delete(id) {
+        const url = `${process.env.VUE_APP_BASE_URL}/v1/quarter/${id}`;
+        return this.sendDeleteRequest(url);
+    }
+}
