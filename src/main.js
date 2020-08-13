fe0dbf2a9b943c7e9602f9610d68396c98b24ea4
Ramazan <laumanov.r@gmail.com>
2020-08-13 15:11:25 +0600

    add lib

--- src/main.js
+++ src/main.js
@@ -1,14 +1,38 @@
 import Vue from 'vue'
 import App from './App.vue'
+import '@/assets/style/main.scss';
 import './registerServiceWorker'
 import router from './_router'
 import store from './_store'
 import vuetify from './plugins/vuetify'
 import VueToast from 'vue-toast-notification';
-import 'vue-toast-notification/dist/theme-default.css';
+import VModal from 'vue-js-modal';
 
-Vue.config.productionTip = false
+import {
+    MdDatepicker,
+    MdButton,
+    MdCheckbox,
+    MdField,
+    MdTable,
+    MdRadio,
+    MdList,
+    MdMenu,
+    MdSwitch,
+} from '../node_modules/vue-material/dist/components';
+
+Vue.config.productionTip = false;
+Vue.use(VModal);
 Vue.use(VueToast, { position: 'top', duration: 3000 });
+Vue.use(MdDatepicker);
+Vue.use(MdButton);
+Vue.use(MdCheckbox);
+Vue.use(MdField);
+Vue.use(MdTable);
+Vue.use(MdRadio);
+Vue.use(MdList);
+Vue.use(MdMenu);
+Vue.use(MdSwitch);
+Vue.material.locale.dateFormat = 'dd.MM.yyyy';
 
 new Vue({
   router,
