6cd69439874c62879533f80a96ddb13094510d54
Ramazan <laumanov.r@gmail.com>
2020-08-14 20:01:36 +0600

    add link to shifts in schedule-week page

---
+++ src/views/school-admin/SchoolAdminScheduleWeek.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="school-admin-schedule">
+        <SuperAdminSchoolHead>
+            <template v-slot:title>
+                Расписание
+            </template>
+            <template v-slot:center>
+                <md-button class="md-primary blue">
+                    <router-link :to="{name: 'shifts'}">Все смены</router-link>
+                </md-button>
+            </template>
+        </SuperAdminSchoolHead>
+
+        <div class="temp-schedule-image">
+            <img src="@/assets/images/schedule-screen.png">
+        </div>
+    </div>
+</template>
+
+<script>
+    import SuperAdminSchoolHead from '@/components/super-admin/schools/SuperAdminSchoolHead';
+    export default {
+        components: {
+            SuperAdminSchoolHead
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .temp-schedule-image {
+        width: 1350px;
+        margin: 0 auto;
+        max-width: 100%;
+        img {
+            width: 100%;
+        }
+    }
+</style>
