ee2c59d98e047b7d03c05c4f020ac33e58a42df5
Ramazan <laumanov.r@gmail.com>
2020-08-21 12:40:09 +0600

    create schedule week service

---
+++ src/_services/schedule-week.service.js
@@ -0,0 +1,28 @@
+import { BaseApiService } from '@/_services/baseApi.service';
+
+export class ScheduleWeekService extends BaseApiService {
+    constructor () {
+        super();
+    }
+
+    getAllBySchoolAndShift(schoolId, shiftId) {
+        const url = `${process.env.VUE_APP_BASE_URL}/v1/instructor/schedule/week/list/school/${schoolId}?shiftId=${shiftId}`;
+        return this.sendGetRequest(url);
+    }
+
+    create(data) {
+        const url = `${process.env.VUE_APP_BASE_URL}/v1/instructor/schedule/week/create`;
+        return this.sendPostRequest(url, data);
+    }
+
+    update(data) {
+        const url = `${process.env.VUE_APP_BASE_URL}/v1/instructor/schedule/week/edit/${data.id}`;
+        return this.sendPostRequest(url, data);
+    }
+
+    deleteSchedule(id) {
+        const url = `${process.env.VUE_APP_BASE_URL}/v1/instructor/schedule/week/${id}`;
+        return this.sendDeleteRequest(url);
+    }
+}
+export default new ScheduleWeekService();
