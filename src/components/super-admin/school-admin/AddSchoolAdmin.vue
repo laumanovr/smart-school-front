8bcd5ba059ef7c3357d9f91a6c6d4d6b8be056d0
Ramazan <laumanov.r@gmail.com>
2020-08-31 13:03:20 +0600

    add class to modal head in add-school-admin

--- src/components/super-admin/school-admin/AddSchoolAdmin.vue
+++ src/components/super-admin/school-admin/AddSchoolAdmin.vue
@@ -154,7 +154,7 @@ export default {
     border-radius: 7px;
     padding: 20px 40px;
 
-    > div {
+    > div.form-head {
         display: flex;
         justify-content: space-between;
         align-items: center;
