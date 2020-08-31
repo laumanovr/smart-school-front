221298f8c468bfbfbe3aa8bc2eca96933915d097
Ramazan <laumanov.r@gmail.com>
2020-08-31 14:15:20 +0600

    create journal grade and all his logic

---
+++ src/views/instructor/InstructorJournalGrade.vue
@@ -0,0 +1,454 @@
+<template>
+    <div class="journal-grade-container">
+        <ClassSelectHeader
+            :headTitle="'Журнал'"
+            :showClass="true"
+            @sendData="getInstructorCourses"
+            @classSelected="onChangeClass"
+        />
+
+        <div class="grade-content">
+            <div class="selects">
+                <h3 class="course-name" v-if="instructorCourses.length">
+                    {{ showCourseName(instructorCourses[0].courseCode) }}
+                </h3>
+                <div class="select-reason">
+                    <v-select
+                        :items="gradeReasons"
+                        item-text="title"
+                        item-value="id"
+                        label="Тип оценки"
+                        v-model="selectedReasonId"
+                    ></v-select>
+                </div>
+            </div>
+
+            <div class="grade-tables" v-if="studentGrades.length">
+                <table class="students bordered">
+                    <thead>
+                    <tr>
+                        <th>
+                            <span class="date">
+                                {{numMonths[currentMonthDays[0].day.slice(3, 5)]}}
+                                {{currentMonthDays[0].day.slice(0, 2)}} -
+                                {{currentMonthDays[currentMonthDays.length - 1].day.slice(0, 2)}}
+                            </span>
+                            <span class="material-icons date-arrow left"
+                                  @click="filterGradesByMonth('prev')">play_arrow</span>
+                            <span class="material-icons date-arrow right" @click="filterGradesByMonth('next')">play_arrow</span>
+                        </th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr v-for="student in studentGrades" :key="student.student.id">
+                        <td>{{ student.student.name }}</td>
+                    </tr>
+                    </tbody>
+                </table>
+                <table class="grades bordered" ref="tableGrades">
+                    <thead>
+                    <tr>
+                        <th v-for="day in currentMonthDays" :class="{'disabled': !isAbleDate(day.day)}">
+                            {{ day.day.slice(0, 2) }}
+                            <div class="lesson">Урок-{{ day.shiftTimeTitle }}</div>
+                        </th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr v-for="student in studentGrades" :key="student.student.id">
+                        <template v-for="day in currentMonthDays">
+                            <td
+                                v-if="getStudentMark(day, student.grades)"
+                                :contenteditable="true"
+                                @keyup="setGradeMark($event, student, day, getStudentMark(day, student.grades))"
+                            >
+                                {{ showCurrentMark(getStudentMark(day, student.grades)) }}
+                            </td>
+                            <td
+                                :class="{'disabled': !isAbleDate(day.day)}"
+                                :contenteditable="isAbleDate(day.day)"
+                                @keyup="setGradeMark($event, student, day)"
+                                v-else>
+                            </td>
+                        </template>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div class="btn-actions" v-if="studentGrades.length">
+                <v-btn color="primary" @click="submitSaveGrades">Сохранить</v-btn>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import ClassSelectHeader from '@/components/instructor/ClassSelectHeader';
+    import GradeService from '@/_services/grade.service';
+    import ScheduleWeekService from '@/_services/schedule-week.service';
+    import moment from 'moment';
+    import GradeReasonService from '@/_services/grade-reason.service';
+    import {AdminCourseService} from '@/_services/admin-course.service';
+    const adminCourseService = new AdminCourseService();
+
+    export default {
+        components: {
+            ClassSelectHeader
+        },
+
+        computed: {
+            userProfile() {
+                return this.$store.state.account.profile
+            },
+            school() {
+                return this.userProfile.schools[0];
+            }
+        },
+
+        data() {
+            return {
+                numMonths: {
+                    '04': 'Апрель',
+                    '05': 'Май',
+                    '06': 'Июнь',
+                    '07': 'Июль',
+                    '08': 'Август',
+                    '09': 'Сентябрь',
+                    '10': 'Октябрь',
+                    '11': 'Ноябрь',
+                    '12': 'Декабрь',
+                },
+                gradeRequest: {
+                    pageRequest: {
+                        limit: 100,
+                        offset: 0
+                    },
+                    searchRequest: {
+                        chronicleId: 0,
+                        instructorId: 0,
+                        from: '',
+                        to: ''
+                    }
+                },
+                monthDataRequest: {
+                    chronicleId: 0,
+                    classId: 0,
+                    courseId: 0,
+                    instructorId: 0,
+                    shiftId: null
+                },
+                step: 0,
+                selectedReasonId: '',
+                instructorCourses: [],
+                currentMonthDays: [],
+                studentGrades: [],
+                sendGradeDtoList: [],
+                gradeReasons: [],
+                gradeMonthFrom: new Date().getMonth(),
+                gradeMonthTo: new Date().getMonth() + 1,
+                scheduleMonthNumber: new Date().getMonth() + 2,
+                allAdminCourses: []
+            }
+        },
+
+        created() {
+            this.fetchAllAdminCourses(); // temp
+        },
+
+        methods: {
+            fetchAllAdminCourses() {
+                adminCourseService.list().then((res) => {
+                    this.allAdminCourses = res;
+                })
+            },
+
+            showCourseName(code) {
+                if (this.allAdminCourses.length) {
+                    return this.allAdminCourses.find(course => course.code === code).title;
+                }
+            },
+
+            getInstructorCourses(schedules) {
+                this.instructorCourses = schedules;
+            },
+
+            async onChangeClass(klass) {
+                if (!klass) {
+                    this.$toast.info('У вас нет классов!');
+                    return;
+                }
+                this.monthDataRequest.classId = klass.classId;
+                this.monthDataRequest.courseId = klass.courseId;
+                this.monthDataRequest.instructorId = this.userProfile.personId;
+                this.monthDataRequest.chronicleId = this.school.chronicleId;
+                this.gradeRequest.searchRequest.instructorId = this.userProfile.personId;
+                this.gradeRequest.searchRequest.chronicleId = this.school.chronicleId;
+                this.gradeRequest.searchRequest.from = this.getFirstDateOfMonth();
+                this.gradeRequest.searchRequest.to = this.getLastDateOfMonth();
+                this.fetchInstructorGradeReasons(klass.classLevel);
+                await this.fetchCurrentMonthSchedule();
+                await this.fetchStudentGrades();
+            },
+
+            fetchStudentGrades() {
+                GradeService.getByClassCourseInstructor(
+                    this.monthDataRequest.classId,
+                    this.monthDataRequest.courseId,
+                    this.gradeRequest
+                ).then((res) => {
+                    this.studentGrades = res.list;
+                }).catch((err) => {
+                    this.$toast.error(err);
+                })
+            },
+
+            fetchCurrentMonthSchedule() {
+                ScheduleWeekService.getCurrentMonth(this.scheduleMonthNumber, this.monthDataRequest)
+                    .then((res) => {
+                        this.currentMonthDays = res.map((item) => {
+                            item.day = moment(item.day, 'YYYY-MM-DD').format('DD.MM.YYYY');
+                            return item;
+                        });
+                    }).catch((err) => {
+                    this.$toast.error(err);
+                })
+            },
+
+            fetchInstructorGradeReasons(classLevel) {
+                GradeReasonService.getByInstructor(
+                    this.userProfile.personId,
+                    classLevel,
+                    this.monthDataRequest.courseId
+                ).then((res) => {
+                    if (res.length) {
+                        this.gradeReasons = res;
+                        this.selectedReasonId = this.gradeReasons[0].id;
+                    }
+                })
+            },
+
+            isAbleDate(gradeDate) {
+                const formattedGradeDate = moment(gradeDate, 'DD.MM.YYYY').format('YYYY-MM-DD');
+                const currentDate = moment(new Date()).format('YYYY-MM-DD');
+                return formattedGradeDate <= currentDate;
+            },
+
+            getStudentMark(dayObj, studentGrades) {
+                return studentGrades.find(grade => grade.date === dayObj.day && grade.shiftTimeId === dayObj.shiftTimeId)
+            },
+
+            showCurrentMark(grade) {
+                return isNaN(grade.mark) ? 'Н' : grade.mark;
+            },
+
+            setGradeMark(e, student, day, currentGrade) {
+                const inputValue = e.target.innerText = e.target.innerText.trim().slice(0, 1);
+
+                let newGradeObj = {
+                    classId: this.monthDataRequest.classId,
+                    gradeId: currentGrade ? currentGrade.gradeId : 0,
+                    m2mStudentCourseId: student.m2mStudentCourseId,
+                    instructorId: this.userProfile.personId,
+                    shiftTimeId: day.shiftTimeId,
+                    gradeDate: day.day,
+                    mark: inputValue,
+                    gradeType: !isNaN(inputValue) ? 'GRADE' : 'ATTENDANCE',
+                    reasonId: currentGrade ? currentGrade.reasonId : this.selectedReasonId,
+                    comment: '',
+                    extraMark: '',
+                    topicId: ''
+                };
+
+                this.sendGradeDtoList.forEach((existGrade, index, selfArr) => {
+                    // DELETE FROM LOCAL ARRAY
+                    let isExist = existGrade.gradeDate === newGradeObj.gradeDate &&
+                        existGrade.shiftTimeId === newGradeObj.shiftTimeId &&
+                        existGrade.m2mStudentCourseId === newGradeObj.m2mStudentCourseId;
+                    if (isExist) {
+                        selfArr.splice(index, 1);
+                    }
+                });
+
+                if (inputValue.length) {
+                    this.sendGradeDtoList.push(newGradeObj);
+                } else {
+                    if (currentGrade) {
+                        // FOR DELETE FROM SERVER
+                        newGradeObj.gradeId = currentGrade.gradeId;
+                        newGradeObj.mark = 99;
+                        this.sendGradeDtoList.push(newGradeObj);
+                    }
+                }
+            },
+
+            filterGradesByMonth(nav) {
+                const gradeTable = this.$refs.tableGrades;
+                let hasScrollBar = gradeTable.scrollWidth > gradeTable.clientWidth;
+                let endOfScroll = gradeTable.scrollWidth - gradeTable.clientWidth;
+                if (nav === 'prev') {
+                    if (hasScrollBar) {
+                        if (this.step) {
+                            this.step -= 300;
+                            gradeTable.scrollLeft = this.step;
+                        } else {
+                            this.step = 0;
+                            this.scrollPrevMonth();
+                            gradeTable.scrollLeft = 0;
+                        }
+                    } else {
+                        this.scrollPrevMonth();
+                    }
+                } else {
+                    if (hasScrollBar) {
+                        if (this.step < endOfScroll) {
+                            this.step += 300;
+                            gradeTable.scrollLeft = this.step;
+                        } else {
+                            this.step = 0;
+                            this.scrollNextMonth();
+                            gradeTable.scrollLeft = 0;
+                        }
+                    } else {
+                        this.scrollNextMonth();
+                    }
+                }
+            },
+
+            async scrollNextMonth() {
+                this.scheduleMonthNumber += 1;
+                this.gradeMonthFrom += 1;
+                this.gradeMonthTo += 1;
+                this.gradeRequest.searchRequest.from = this.getFirstDateOfMonth();
+                this.gradeRequest.searchRequest.to = this.getLastDateOfMonth();
+                await this.fetchCurrentMonthSchedule();
+                await this.fetchStudentGrades();
+            },
+
+            async scrollPrevMonth() {
+                this.scheduleMonthNumber -= 1;
+                this.gradeMonthFrom -= 1;
+                this.gradeMonthTo -= 1;
+                this.gradeRequest.searchRequest.from = this.getFirstDateOfMonth();
+                this.gradeRequest.searchRequest.to = this.getLastDateOfMonth();
+                await this.fetchCurrentMonthSchedule();
+                await this.fetchStudentGrades();
+            },
+
+            getFirstDateOfMonth() {
+                const date = new Date();
+                return moment(new Date(date.getFullYear(), this.gradeMonthFrom, 1)).format('DD.MM.YYYY');
+            },
+
+            getLastDateOfMonth() {
+                const date = new Date();
+                return moment(new Date(date.getFullYear(), this.gradeMonthTo, 0)).format('DD.MM.YYYY');
+            },
+
+            submitSaveGrades() {
+                if (!this.sendGradeDtoList.length) {
+                    this.$toast.info('Сначала поставьте оценку');
+                    return;
+                }
+                GradeService.createWithArray(this.sendGradeDtoList).then(() => {
+                    this.sendGradeDtoList = [];
+                    this.$toast.success('Успешно');
+                }).catch((err) => {
+                    this.$toast.error(err);
+                })
+            },
+
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .journal-grade-container {
+        margin: 30px;
+        .grade-content {
+            .selects {
+                display: flex;
+                align-items: center;
+                margin: 20px 0;
+                .course-name {
+
+                }
+                .select-reason {
+                    margin-left: 20px;
+                }
+            }
+            .grade-tables {
+                display: flex;
+                table.students {
+                    border-top: 0;
+                    th {
+                        color: #03A9F4;
+                        display: flex;
+                        align-items: center;
+                        border-right: 0;
+                        .date {
+                            white-space: nowrap;
+                        }
+                        .date-arrow {
+                            color: #a8a8a8;
+                            font-size: 18px;
+                            border: 1px solid #d1d1d1;
+                            padding: 8px;
+                            transform: translateX(10px);
+                            border-radius: 4px;
+                            cursor: pointer;
+                            &.left {
+                                margin: 0 8px;
+                                transform: rotate(180deg) translateX(-10px);
+                            }
+                        }
+                    }
+                    td {
+                        white-space: nowrap;
+                    }
+                }
+                table.grades {
+                    max-width: 100%;
+                    overflow-x: auto;
+                    display: inline-block;
+                    scroll-behavior: smooth;
+                    border-top: 0;
+                    /*&::-webkit-scrollbar {*/
+                    /*width: 0;*/
+                    /*height: 0;*/
+                    /*background: transparent;*/
+                    /*}*/
+                    /*&::-webkit-scrollbar-thumb {*/
+                    /*background-color: rgba(0, 0, 0, 0.3)*/
+                    /*}*/
+                    th, td {
+                        width: 50px;
+                        max-width: 50px;
+                        min-width: 50px;
+                        white-space: nowrap;
+                        overflow: hidden;
+                        text-align: center;
+                    }
+                    th {
+                        color: #03A9F4;
+                        padding: 0;
+                        .lesson {
+                            font-size: 11px;
+                            color: rgba(74, 39, 243, 0.51);
+                        }
+                    }
+                    td {
+                        padding: 0 15px;
+                        outline: none;
+                        &:focus {
+                            background: #fafafa;
+                            box-shadow: 0 0 20px 0 rgba(74, 39, 243, 0.51);
+                        }
+                    }
+                }
+            }
+            .disabled {
+                background: #eaeaea;
+            }
+        }
+    }
+</style>
