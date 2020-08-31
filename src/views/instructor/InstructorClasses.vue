50380974d8818b00bdb5a7252dbfc756e314c34d
Ramazan <laumanov.r@gmail.com>
2020-08-31 14:44:52 +0600

    add preloader to instr classes

--- src/views/instructor/InstructorClasses.vue
+++ src/views/instructor/InstructorClasses.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="instructor-class-container">
+        <PreLoader v-if="isLoading"/>
         <ClassSelectHeader :headTitle="'Мои Классы'" :showClass="false" />
 
         <div class="class-content" v-if="showData">
@@ -30,13 +31,16 @@
     const instructorClassService = new InstructorClassService();
     import {AdminCourseService} from '@/_services/admin-course.service';
     const adminCourseService = new AdminCourseService();
+    import PreLoader from "@/components/preloader/PreLoader";
 
     export default {
         components: {
-            ClassSelectHeader
+            ClassSelectHeader,
+            PreLoader
         },
         data() {
             return {
+                isLoading: false,
                 showData: false,
                 instrClasses: [],
                 allSchoolClasses: [],
@@ -51,9 +55,10 @@
         },
 
         async created() {
+            this.isLoading = true;
             await this.fetchAllSchoolClasses();
             await this.fetchAllAdminCourses();
-            this.fetchInstructorClasses();
+            await this.fetchInstructorClasses();
         },
 
         methods: {
@@ -64,8 +69,10 @@
                             (el['classId'] === obj['classId'])
                         ));
                     this.showData = true;
+                    this.isLoading = false;
                 }).catch(err => {
                     this.$toast.error(err);
+                    this.isLoading = false;
                 })
             },
 
