30d730b0a947fc1704adea19745f6d51abe31b24
Ramazan <laumanov.r@gmail.com>
2020-09-03 11:09:38 +0600

    add function to add several courses to one teacher

--- src/views/school-admin/Teachers.vue
+++ src/views/school-admin/Teachers.vue
@@ -42,16 +42,23 @@
 	            <th>ID</th>
                 <th>Ф.И.О</th>
                 <th>Предмет</th>
-                <!--<th>Логин</th>-->
+                <th>Логин</th>
                 <th><img alt="" src="../../assets/images/icons/plus.svg"></th>
+                <th></th>
             </template>
 
             <template v-slot:body="{ item }">
                 <td>{{ (currentPage - 1) * 10 + item.index + 1 }}</td>
 	            <td>{{ item.firstName+' '+item.lastName }}</td>
-                <td><span v-for="courseCode in item.courses">{{showCourseName(courseCode)}}</span>,</td>
+                <td class="instr-courses">
+                    <template v-if="item.courses.length">
+                        <span v-for="courseCode in item.courses">{{showCourseName(courseCode)}},</span>
+                    </template>
+                    <span v-else></span>
+                </td>
                 <td>{{ item.username }}</td>
                 <td @click="editUser(item)" class="actions"><img alt="" src="../../assets/images/icons/pen.svg"></td>
+                <td><CourseIcon @click="addCourseModal(item)"/></td>
             </template>
         </SmartTable>
         <v-dialog
@@ -65,6 +72,42 @@
         <v-dialog v-if="isAddFile" v-model="isAddFile" width="546" id="add-file">
             <ImportFile @submit="onSubmit"></ImportFile>
         </v-dialog>
+
+        <!--ADD COURSE MODAL-->
+        <modal name="add-course-modal" height="auto">
+            <div class="modal-container">
+                <div class="course-content">
+                    <div class="added">
+                        <h4>Добавленные:</h4>
+                        <div class="teacher-courses">
+                            <div class="course" v-for="(instrCourse, i) in teacherCourses" :key="i">
+                                <span>{{ showCourseName(instrCourse.courseName) }}</span>
+                                <DeleteIcon @click="deleteTeacherCourse(instrCourse.id, i)"/>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="add-new">
+                        <v-form ref="addCourseForm">
+                        <div>
+                            <v-select
+                                :rules="required"
+                                :items="schoolCourses"
+                                item-text="courseTitle"
+                                item-value="id"
+                                label="Добавить предмет"
+                                v-model="addTeacherCourse.courseId"
+                            >
+                            </v-select>
+                        </div>
+                        <div class="btn-actions">
+                            <v-btn color="red" @click="$modal.hide('add-course-modal')">Отмена</v-btn>
+                            <v-btn color="green" @click="submitAddTeacherCourse">Сохранить</v-btn>
+                        </div>
+                        </v-form>
+                    </div>
+                </div>
+            </div>
+        </modal>
     </div>
 </template>
 
@@ -89,14 +132,22 @@ import {AdminCourseService} from '@/_services/admin-course.service';
 const adminCourseService = new AdminCourseService();
 import InstructorService from "@/_services/instructor.service";
 import PreLoader from "@/components/preloader/PreLoader";
+import CourseIcon from '@/components/icons/CourseIcon';
+import DeleteIcon from '@/components/icons/DeleteIcon';
+import {CourseService} from '@/_services/course.service';
+const courseService = new CourseService();
+
 export default {
     name: 'Teachers',
     components: {
 	    PreLoader,
         ImportFile,
         ExcelJs,
+        CourseIcon,
+        DeleteIcon,
         SmartSelect, SmartBtn2, SmartSearchInput, SmartButton, AddTeacher, SuperAdminSchoolHead, SmartTable},
     data: () => ({
+        required: [v => !!v || 'Обязательное поле'],
         isAddUser: false,
         users: [],
         user: {},
@@ -109,7 +160,16 @@ export default {
         pageSize: 0,
         currentPage: 1,
         allAdminCourses: [],
-	    isLoading: false
+	    isLoading: false,
+        schoolCourses: [],
+        allSchoolCourses: [],
+        teacherCourses: [],
+        addTeacherCourse: {
+            archived: false,
+            courseId: 0,
+            personId: 0,
+            schoolId: 0
+        }
     }),
     computed: {
         userProfile() {
@@ -117,8 +177,10 @@ export default {
         }
     },
     async mounted() {
+        this.addTeacherCourse.schoolId = this.userProfile.schools[0].id;
         await this.fetchAllAdminCourses();
         this.fetchUsers();
+        this.fetchSchoolCourses();
     },
     methods: {
         fetchAllAdminCourses() {
@@ -130,6 +192,12 @@ export default {
             if (code) {
                 return this.allAdminCourses.find(course => course.code === code).title;
             }
+        },
+        fetchSchoolCourses() {
+            courseService.listBySchool(this.userProfile.schools[0].id).then((res) => {
+                this.schoolCourses = res;
+                this.allSchoolCourses = res;
+            });
         },
         onAddAdmin() {
             this.isAddUser = true
@@ -218,10 +286,98 @@ export default {
             this.currentPage++;
             this.fetchUsers(this.currentPage - 1);
         },
+
+        addCourseModal(instructor) {
+            this.isLoading = true;
+            this.addTeacherCourse.courseId = '';
+            this.addTeacherCourse.personId = instructor.id;
+            this.schoolCourses = this.allSchoolCourses;
+            instructorCourseService.listByInstructor(instructor.id).then((res) => {
+                this.teacherCourses = res._embedded ? res._embedded.instructorCourseResourceList : [];
+                if (this.teacherCourses.length) {
+                    const existCourses = this.teacherCourses.map(course => course.courseId);
+                    existCourses.forEach((existCourseId) => {
+                        this.schoolCourses = this.schoolCourses.filter(course => course.id !== existCourseId);
+                    });
+                }
+                this.isLoading = false;
+                this.$modal.show('add-course-modal');
+            }).catch((err) => {
+                this.$toast.error(err);
+                this.isLoading = false;
+            });
+        },
+
+        submitAddTeacherCourse() {
+            if (this.$refs.addCourseForm.validate()) {
+                this.isLoading = true;
+                instructorCourseService.create(this.addTeacherCourse).then(() => {
+                    this.fetchUsers();
+                    this.isLoading = false;
+                    this.$toast.success('Успешно добавлено!');
+                    this.$modal.hide('add-course-modal');
+                }).catch((err) => {
+                    this.$toast.error(err);
+                    this.isLoading = false;
+                });
+            }
+        },
+
+        deleteTeacherCourse(instrCourseId, index) {
+            this.isLoading = true;
+            instructorCourseService._delete(instrCourseId).then(() => {
+                this.isLoading = false;
+                this.teacherCourses.splice(index, 1);
+                this.$toast.success('Успешно удалено!');
+                this.fetchUsers();
+            }).catch((err) => {
+                this.$toast.error(err);
+                this.isLoading = false;
+            });
+        }
     }
 }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
+    .school-admin-teachers {
+        .v-btn__content {
+            color: #fff;
+        }
+        .instr-courses {
+            span {
+                margin-right: 5px;
+            }
+        }
+        .modal-container {
+            .course-content {
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                .added {
+                    border-right: 2px solid #9E9E9E;
+                    margin-right: 20px;
+                    padding-right: 20px;
+                    min-width: 45%;
+                }
+                .add-new {
+                    min-width: 55%;
+                }
 
+                .teacher-courses {
+                    text-align: center;
+                    transform: translateY(-10px);
+                    .course {
+                        margin-bottom: 8px;
+                    }
+                    .delete-icon {
+                        width: 22px;
+                        min-width:  22px;
+                        height: 22px;
+                        margin-left: 10px;
+                    }
+                }
+            }
+        }
+    }
 </style>
