7af6770af09e6ee19a300131ec7a536e11004cef
Ramazan <laumanov.r@gmail.com>
2020-09-03 11:02:53 +0600

    create course icon

---
+++ src/components/icons/CourseIcon.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="course-icon" @click="$emit('click')">
+        <img src="@/assets/images/icons/courses.svg" title="Добавить предмет">
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    .course-icon {
+        display: inline-block;
+        cursor: pointer;
+        width: 22px;
+        min-width: 22px;
+        height: 22px;
+        img {
+            padding: 3px;
+            border-radius: 5px;
+            background: #90a1ff;
+        }
+    }
+</style>
