20def644ab627ae48919852142903aa94c7a8076
Ramazan <laumanov.r@gmail.com>
2020-09-09 15:30:46 +0600

    add query params to instructor service

--- src/_services/instructor.service.js
+++ src/_services/instructor.service.js
@@ -5,8 +5,8 @@ export class InstructorService extends BaseApiService {
         super();
     }
 
-    list (page=0, schoolId='') {
-        const url = `${process.env.VUE_APP_BASE_URL}/v1/instructor/list?page=${page}&schoolId=${schoolId}`;
+    list (page=0, schoolId='', courseCode='') {
+        const url = `${process.env.VUE_APP_BASE_URL}/v1/instructor/list?page=${page}&schoolId=${schoolId}&courseCode=${courseCode}`;
         return this.sendGetRequest(url);
     }
 }
