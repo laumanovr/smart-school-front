415bb90fca4b1016a876658a58093d8bacfb5d20
Ramazan <laumanov.r@gmail.com>
2020-09-11 10:52:11 +0600

    add new api to instructor-service

--- src/_services/instructor.service.js
+++ src/_services/instructor.service.js
@@ -5,9 +5,15 @@ export class InstructorService extends BaseApiService {
         super();
     }
 
-    list (page=0, schoolId='', courseCode='') {
-        const url = `${process.env.VUE_APP_BASE_URL}/v1/instructor/list?page=${page}&schoolId=${schoolId}&courseCode=${courseCode}`;
+    list (page=0, schoolId='', courseCode='', regionId='', rayonId='') {
+        const baseUrl = `${process.env.VUE_APP_BASE_URL}/v1/instructor/list`;
+        const url = baseUrl + `?page=${page}&schoolId=${schoolId}&courseCode=${courseCode}&regionId=${regionId}&rayonId=${rayonId}`;
         return this.sendGetRequest(url);
+    }
+
+    responseList(body) {
+        const url = `${process.env.VUE_APP_BASE_URL}/v1/instructor/response/list`;
+        return this.sendPostRequest(url, body);
     }
 }
 
