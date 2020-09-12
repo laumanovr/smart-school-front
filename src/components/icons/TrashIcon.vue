686acd2234e84174303342312edb10e6efe7de35
Ramazan <laumanov.r@gmail.com>
2020-09-12 16:38:49 +0600

    create trashIcon component

---
+++ src/components/icons/TrashIcon.vue
@@ -0,0 +1,19 @@
+<template>
+    <span class="delete-icon" @click="$emit('click')">
+        <img src="@/assets/images/icons/trash.svg" alt="Удалить">
+    </span>
+</template>
+
+<style lang="scss" scoped>
+    .delete-icon {
+        display: inline-block;
+        cursor: pointer;
+        width: 16px;
+        min-width: 16px;
+        height: 16px;
+        img {
+            width: 100%;
+            height: 100%;
+        }
+    }
+</style>
