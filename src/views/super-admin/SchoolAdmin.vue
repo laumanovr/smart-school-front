93b4eb7366316cbbee4b2dc30dd48e2cb10481be
Ramazan <laumanov.r@gmail.com>
2020-09-12 16:36:50 +0600

    add clear select to school-admins in sup-adm

--- src/views/super-admin/SchoolAdmin.vue
+++ src/views/super-admin/SchoolAdmin.vue
@@ -14,37 +14,45 @@
             <template v-slot:firstItem>
                 <div>
                     <div class="select-filter-block">
-                        <v-select
-                            :items="currentRegions"
-                            item-text="title"
-                            item-value="id"
-                            label="Регион"
-                            v-model="filterObj.regionId"
-                            @change="fetchRayonsByRegion"
-                        />
-                        <v-select
-                            :items="filteredRayons"
-                            item-text="title"
-                            item-value="id"
-                            label="Район"
-                            v-model="filterObj.rayonId"
-                            @change="onSelectRayon"
-                        />
-                        <v-select
-                            :items="filteredSchools"
-                            item-text="name"
-                            item-value="id"
-                            label="Школа"
-                            v-model="filterObj.schoolId"
-                            :menu-props="{contentClass: 'schoolSelect'}"
-                            @click="addScrollListenerSchoolSelect"
-                            @change="removeSchoolSelectScrollListener"
-                            @blur="removeSchoolSelectScrollListener"
-                        />
+                        <div>
+                            <div class="select-clear-block">
+                                <v-select
+                                    :items="currentRegions"
+                                    item-text="title"
+                                    item-value="id"
+                                    label="Регион"
+                                    v-model="filterObj.regionId"
+                                    @change="fetchRayonsByRegion"
+                                />
+                                <TrashIcon @click="clearSelect('region')" v-show="filterObj.regionId"/>
+                            </div>
+                            <div class="select-clear-block">
+                                <v-select
+                                    :items="filteredRayons"
+                                    item-text="title"
+                                    item-value="id"
+                                    label="Район"
+                                    v-model="filterObj.rayonId"
+                                    @change="onSelectRayon"
+                                />
+                                <TrashIcon @click="clearSelect('rayon')" v-show="filterObj.rayonId"/>
+                            </div>
+                            <div class="select-clear-block">
+                                <v-select
+                                    :items="filteredSchools"
+                                    item-text="name"
+                                    item-value="id"
+                                    label="Школа"
+                                    v-model="filterObj.schoolId"
+                                    :menu-props="{contentClass: 'schoolSelect'}"
+                                    @click="addScrollListenerSchoolSelect"
+                                    @change="removeSchoolSelectScrollListener"
+                                    @blur="removeSchoolSelectScrollListener"
+                                />
+                                <TrashIcon @click="filterObj.schoolId=''" v-show="filterObj.schoolId"/>
+                            </div>
+                        </div>
                         <v-btn color="primary" @click="filterSchoolAdmins">Фильтр</v-btn>
-                    </div>
-                    <div class="showAll" v-if="showAllBtn">
-                        <button @click="fetchAllSchoolAdminsWithoutFilter">Показать все</button>
                     </div>
                 </div>
             </template>
@@ -88,10 +96,20 @@ import { RayonService } from "@/_services/rayon.service";
 const rayonService = new RayonService();
 import {SchoolService} from '@/_services/school.service';
 const schoolService = new SchoolService();
+import TrashIcon from '@/components/icons/TrashIcon';
 
 export default {
     name: 'Instructors',
-    components: {SmartSelect, SmartSearchInput, SmartButton, SuperAdminSchoolHead, SmartTable, AddSchoolAdmin, PreLoader},
+    components: {
+        SmartSelect,
+        SmartSearchInput,
+        SmartButton,
+        SuperAdminSchoolHead,
+        SmartTable,
+        AddSchoolAdmin,
+        PreLoader,
+        TrashIcon
+    },
     data: () => ({
         isAddAdmin: false,
         users: [],
@@ -107,7 +125,6 @@ export default {
         },
         filteredRayons: [],
         filteredSchools: [],
-        showAllBtn: false
     }),
     computed: {
         currentRegions() {
@@ -185,16 +202,18 @@ export default {
         },
 
         filterSchoolAdmins() {
-            this.showAllBtn = true;
             this.fetchSchoolAdmins();
         },
 
-        fetchAllSchoolAdminsWithoutFilter() {
-            this.filterObj.regionId = '';
-            this.filterObj.rayonId = '';
-            this.filterObj.schoolId = '';
-            this.showAllBtn = false;
-            this.fetchSchoolAdmins();
+        clearSelect(selectName) {
+            if (selectName === 'region') {
+                this.filterObj.regionId = '';
+                this.filterObj.rayonId = '';
+                this.filterObj.schoolId = '';
+            } else {
+                this.filterObj.rayonId = '';
+                this.filterObj.schoolId = '';
+            }
         },
 
         onEdit(item) {
