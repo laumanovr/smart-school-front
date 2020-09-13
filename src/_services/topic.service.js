a641bf32fbfd2867436bda49e742724d76cdd213
Ramazan <laumanov.r@gmail.com>
2020-09-13 12:09:48 +0600

    refactoring in topic-service

--- src/_services/topic.service.js
+++ src/_services/topic.service.js
@@ -4,28 +4,28 @@ export  class TopicService extends BaseApiService {
 	constructor () {
 		super();
 	}
-	
+
 	getById (id) {
 		const url = `${process.env.VUE_APP_BASE_URL}/v1/topic/${id}`;
 		return this.sendGetRequest(url);
 	}
-	
+
 	_delete (id) {
 		const url = `${process.env.VUE_APP_BASE_URL}/v1/topic/${id}`;
 		return this.sendDeleteRequest(url);
 	}
-	
+
 	create (data) {
 		const url = `${process.env.VUE_APP_BASE_URL}/v1/topic/create`;
 		return this.sendPostRequest(url, data);
 	}
-	
+
 	edit (data) {
 		const url = `${process.env.VUE_APP_BASE_URL}/v1/topic/edit/${data.id}`;
 		return this.sendPostRequest(url, data);
 	}
-	
-	getByInstructor (insId, courseId, page, quarterId='') {
+
+	getByInstructor (page, insId, courseId, quarterId='') {
 		const url = `${process.env.VUE_APP_BASE_URL}/v1/topic/list/instructor/${insId}?courseId=${courseId}&page=${page}&quarterId=${quarterId}`;
 		return this.sendGetRequest(url);
 	}
