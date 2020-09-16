f2c8fa836d4566819412ee71de961b66d88b1fc2
Ramazan <laumanov.r@gmail.com>
2020-09-16 12:37:32 +0600

    changed input field in shift-create

--- src/components/shift/ShiftCreateEdit.vue
+++ src/components/shift/ShiftCreateEdit.vue
@@ -1,15 +1,12 @@
 <template>
-<form class="shift">
-    <h4>{{mode == 'addShift' ? 'Добавление смены' : 'Редактирование смены'}}</h4>
-    <md-field>
-        <label>Название смены</label>
-        <md-input v-model="shiftObj.name" required />
-    </md-field>
-    <div class="btn-actions" v-if="mode == 'addShift'">
-        <v-btn color="red" @click="closeModal">Отмена</v-btn>
-        <v-btn color="green" @click="createShift" :disabled="!shiftObj.name">Далее</v-btn>
-    </div>
-</form>
+    <form class="shift">
+        <h4>{{mode == 'addShift' ? 'Добавление смены' : 'Редактирование смены'}}</h4>
+        <v-text-field label="Название смены" v-model="shiftObj.name"/>
+        <div class="btn-actions" v-if="mode == 'addShift'">
+            <v-btn color="red" @click="closeModal">Отмена</v-btn>
+            <v-btn color="green" @click="createShift" :disabled="!shiftObj.name">Далее</v-btn>
+        </div>
+    </form>
 </template>
 
 <script>
