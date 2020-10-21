2e029958709f721fbe4815dcd833ac93759a4881
Ramazan <laumanov.r@gmail.com>
2020-10-21 16:38:41 +0600

    create main component for rayon-head-manage

---
+++ src/views/rayon-head/RayonHeadManage.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="rayon-head-manage">
+        <SuperAdminHeader>
+            <template v-slot:title>{{ fullRayonTitle }}</template>
+            <template v-slot:userName v-if="userProfile.user">{{ userProfile.user.firstName }} {{ userProfile.user.lastName }}</template>
+            <template v-slot:roleName>Глава Района</template>
+        </SuperAdminHeader>
+        <div class="rayon-head-manage__container">
+            <Sidebar :role="'rayonHead'"/>
+            <div class="rayon-head-manage__body" id="rayon-head-manage__body">
+                <router-view></router-view>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import SuperAdminHeader from '@/components/navigations/sidebars/SuperAdminHeader';
+    import Sidebar from '@/components/navigations/sidebars/Sidebar';
+    import {RayonService} from '@/_services/rayon.service';
+    const rayonService = new RayonService();
+
+    export default {
+        name: 'SchoolAdminManage',
+        components: { Sidebar, SuperAdminHeader },
+        data() {
+            return {
+                fullRayonTitle: ''
+            }
+        },
+        computed: {
+            userProfile () {
+                return this.$store.state.account.profile;
+            },
+            headRayonId() {
+                return this.userProfile.rayons ? this.userProfile.rayons[0] : 0;
+            }
+        },
+        mounted() {
+            this.$nextTick(() => {
+                if (this.headRayonId) {
+                    rayonService.getById(this.headRayonId).then((res) => {
+                        this.fullRayonTitle = `${res.title} - ${res.region.title}`;
+                    }).catch((err) => {
+                        this.$toast.error(err);
+                    })
+                }
+            });
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .rayon-head-manage {
+        background: #f8f8fb;
+
+        &__container {
+            display: flex;
+            height: calc(100vh - 76px);
+        }
+        &__body {
+            width: calc(100% - 96px);
+            overflow-y: auto;
+        }
+    }
+</style>
