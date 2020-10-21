ea0a5f80ca36de16afa206e57dc2bc40e3610fda
Ramazan <laumanov.r@gmail.com>
2020-10-21 16:30:16 +0600

    create page calendar for rayon-head panel

---
+++ src/views/rayon-head/Calendar.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="calendar">
+        <calendar-view></calendar-view>
+    </div>
+</template>
+
+<script>
+    import calendarView from '@/components/super-admin/calendar/Calendar'
+    export default {
+        name: "Calendar",
+        components: { calendarView }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .calendar {
+        margin-top: 20px;
+    }
+</style>
