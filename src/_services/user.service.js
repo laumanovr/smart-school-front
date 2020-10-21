5bb71f88061f25fa17f146aaf04eabb9f828cf4f
Ramazan <laumanov.r@gmail.com>
2020-10-21 08:39:02 +0600

    add functions to register and update user

--- src/_services/user.service.js
+++ src/_services/user.service.js
@@ -9,15 +9,35 @@ export const userService = {
 		};
 		return fetch(`${process.env.VUE_APP_BASE_URL}/v1/auth/login`, options).then(handleResponse);
 	},
+
 	logout () {
 		localStorage.removeItem('user');
 		localStorage.removeItem('profile');
 	},
+
 	getProfile () {
 		const options = {
 			method: 'GET',
 			headers: authHeader(),
 		};
 		return fetch(`${process.env.VUE_APP_BASE_URL}/v1/users/profile`, options).then(handleResponse);
-	}
+	},
+
+    registerUser(body) {
+        const options = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(body)
+        };
+        return fetch(`${process.env.VUE_APP_BASE_URL}/v1/auth/register`, options).then(handleResponse);
+    },
+
+    updateUser(body) {
+        const options = {
+            method: 'POST',
+            headers: {...authHeader(), 'Content-Type': 'application/json'},
+            body: JSON.stringify(body)
+        };
+        return fetch(`${process.env.VUE_APP_BASE_URL}/v1/users/edit/${body.id}`, options).then(handleResponse);
+    }
 };
