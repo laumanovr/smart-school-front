90b7b8ea55843c35b8f98d13adf03959b056db45
Ramazan <laumanov.r@gmail.com>
2020-10-26 10:45:10 +0600

    add quarter-average func in analytics-service

--- src/_services/analytics.service.js
+++ src/_services/analytics.service.js
@@ -10,6 +10,12 @@ export class AnalyticsService extends BaseApiService {
         const url = baseUrl + `?startDate=${startDate}&endDate=${endDate}&roleId=${roleId}`;
         return this.sendGetRequest(url);
     }
+
+    quarterAverage(chronicleId, schoolId='', quarterId='', rayonId='') {
+        const baseUrl = `${process.env.VUE_APP_BASE_URL}/v1/analytic/quarter-grade`;
+        const url = baseUrl + `?chronicleId=${chronicleId}&schoolId=${schoolId}&quarterId=${quarterId}&rayonId=${rayonId}`;
+        return this.sendGetRequest(url);
+    }
 }
 
 export default new AnalyticsService();
