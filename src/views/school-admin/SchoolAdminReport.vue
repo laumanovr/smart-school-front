c06dfda2e3be0838f4985df185cfa1a7b3f2cc56
Ramazan <laumanov.r@gmail.com>
2020-10-27 13:29:40 +0600

    create school-admin-report component

---
+++ src/views/school-admin/SchoolAdminReport.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="report">
+        <h2>Admin report</h2>
+    </div>
+</template>
+
+<script>
+    import AnalyticsService from '@/_services/analytics.service';
+
+    export default {
+        data() {
+            return {
+
+            }
+        },
+
+        methods: {
+
+        }
+    }
+</script>
