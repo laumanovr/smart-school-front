8e9b8e3938f077733f38959e749ead7d49d385a8
Ramazan <laumanov.r@gmail.com>
2020-10-27 13:32:16 +0600

    create instructor-report

---
+++ src/views/instructor/InstructorReport.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="instructor-report-container">
+        <ClassQualityKnowledgeReport/>
+    </div>
+</template>
+
+<script>
+    import ClassQualityKnowledgeReport from '@/components/report/ClassQualityKnowledgeReport';
+    export default {
+        components: {
+            ClassQualityKnowledgeReport
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+.instructor-report-container {
+    margin: 25px;
+}
+</style>
