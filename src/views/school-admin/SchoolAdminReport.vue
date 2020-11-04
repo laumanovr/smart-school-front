471b867a26fd2d96d7173f815ce4fcd9d74146be
Ramazan <laumanov.r@gmail.com>
2020-11-04 08:55:12 +0600

    add select reports in admin

--- src/views/school-admin/SchoolAdminReport.vue
+++ src/views/school-admin/SchoolAdminReport.vue
@@ -1,14 +1,73 @@
 <template>
     <div class="admin-report-container">
-        <AllClassesQualityReport/>
+        <div class="header-title">
+            <h3>Отчеты</h3>
+            <v-select
+                class="v-select-item"
+                placeholder="Выбрать"
+                :items="reportTypes"
+                item-text="title"
+                item-value="type"
+                @change="changeReport"
+            />
+        </div>
+        <template v-if="reportType == 'performance'">
+            <AllClassesQualityReport
+                :allChronicleYears="allChronicleYears"
+            />
+        </template>
+        <template v-if="reportType == 'activity'">
+            <AllClassActivityReport
+                :allChronicleYears="allChronicleYears"
+            />
+        </template>
     </div>
 </template>
 
 <script>
     import AllClassesQualityReport from '@/components/report/AllClassesQualityReport';
+    import AllClassActivityReport from '@/components/report/AllClassActivityReport';
+    import {ChronicleService} from '@/_services/chronicle.service';
+    const chronicleService = new ChronicleService();
+
     export default {
         components: {
-            AllClassesQualityReport
+            AllClassesQualityReport,
+            AllClassActivityReport
+        },
+        data() {
+            return {
+                reportType: '',
+                allChronicleYears: [],
+                reportTypes: [
+                    {title: 'Отчет по движению', type: 'activity'},
+                    {title: 'Отчет по качеству знаний', type: 'performance'}
+                ]
+            }
+        },
+        computed: {
+            userProfile() {
+                return this.$store.state.account.profile
+            },
+            school() {
+                return this.userProfile.schools[0];
+            }
+        },
+        created() {
+            this.getAllChronicleYears();
+        },
+        methods: {
+            getAllChronicleYears() {
+                chronicleService.list().then((res) => {
+                    this.allChronicleYears = res;
+                }).catch((err) => {
+                    this.$toast.error(err);
+                })
+            },
+
+            changeReport(type) {
+                this.reportType = type;
+            }
         }
     }
 </script>
@@ -16,5 +75,12 @@
 <style lang="scss">
     .admin-report-container {
         margin: 25px;
+        .header-title {
+            display: flex;
+            align-items: center;
+            .v-select-item {
+                margin-left: 25px;
+            }
+        }
     }
 </style>
