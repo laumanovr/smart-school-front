15c893fc8b0a9bb7968dd4d386ea4cef00e5c1ba
Ramazan <laumanov.r@gmail.com>
2020-11-13 12:34:01 +0600

    add props for selected school in activity-report for rayon-head

--- src/components/report/AllClassActivityReport.vue
+++ src/components/report/AllClassActivityReport.vue
@@ -79,7 +79,8 @@
         },
         props: {
             allChronicleYears: Array,
-            schoolQuarters: Array
+            schoolQuarters: Array,
+            selectedSchool: Object
         },
         data() {
             return {
@@ -99,7 +100,7 @@
                 return this.$store.state.account.profile
             },
             school() {
-                return this.userProfile.schools[0];
+                return this.userProfile.schools ? this.userProfile.schools[0] : this.selectedSchool;
             }
         },
         methods: {
