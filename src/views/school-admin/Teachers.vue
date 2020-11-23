aafa73eb3c007d6d6cb5222f4170a99367cb31ed
Ramazan <laumanov.r@gmail.com>
2020-11-23 11:44:23 +0600

    refactor dob instr edit

--- src/views/school-admin/Teachers.vue
+++ src/views/school-admin/Teachers.vue
@@ -328,7 +328,7 @@ export default {
                     phone: res.phone,
                     courseId: item.courseId,
                     pin: res.pin,
-                    birthday: res.dateOfBirth ? res.dateOfBirth : '11.10.1970'
+                    birthday: res.dateOfBirth
                 };
                 this.isAddUser = true;
                 this.isEdit = true;
