488372d9b7628fd705f44ae05e58b34a3b45e4ff
Ramazan <laumanov.r@gmail.com>
2020-11-30 12:03:10 +0600

    create support page for landing

---
+++ src/views/landing/Support.vue
@@ -0,0 +1,631 @@
+<template>
+    <div>
+        <Navbar/>
+        <div class="content">
+            <div class="search">
+                <span class="fa fa-search form-control-feedback"></span>
+                <input type="text" class="form-control" ref="SearchInput"
+                       placeholder="Search answers related your questions">
+            </div>
+            <div class="popular">
+                POPULAR
+            </div>
+            <div class="row">
+                <video class="PopularsVideoDisplayNone" width="320" height="240" controls>
+                    <source src="https://www.youtube.com/watch?v=Jb1_hbjD8tQ" type="video/mp4">
+                    <source src="https://www.youtube.com/watch?v=Jb1_hbjD8tQ" type="video/ogg">
+                    Your browser does not support the video tag.
+                </video>
+                <div class="col-5">
+                    <h3>Feugiat sodales at nullam tellus.</h3>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mi posuere nibh tempor fringilla porta pellentesque ipsum.</p>
+                    <div class="info-item">
+                    <span class="adressactor">Samatha Leuthex</span>
+                        <span class="infodata">May 5, 3 min read</span>
+                    </div>
+                    <div class="popularinfodown">
+                        <img src="@/assets/images/landing/savesimvol.png">
+                        <img src="@/assets/images/landing/sharesimvol.png">
+                    </div>
+                </div>
+                <div class="col-6">
+                    <img src="@/assets/images/landing/videos.png">
+                </div>
+            </div>
+            <div class="flexbudet">
+                <div>
+                    <img class="videoimg" src="@/assets/images/landing/isakov.png">
+                    <h4>Porta proin tellus sed pulvinar odio eu.</h4>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+                    <div class="info-item"><span class="adressactor">Samatha Leuthex</span>
+                        <span class="infodata">May 5, 3 min read</span>
+                    </div>
+                    <div class="infodown">
+                        <img src="@/assets/images/landing/savesimvol.png">
+                        <img src="@/assets/images/landing/sharesimvol.png">
+                    </div>
+                </div>
+                <div>
+                    <img class="videoimg" src="@/assets/images/landing/isakov.png">
+                    <h4>Porta proin tellus sed pulvinar odio eu.</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+                    <div class="info-item"><span class="adressactor">Samatha Leuthex</span>
+                        <span class="infodata">May 5, 3 min read</span>
+                    </div>
+                    <div class="infodown">
+                        <img src="@/assets/images/landing/savesimvol.png">
+                        <img src="@/assets/images/landing/sharesimvol.png">
+                    </div>
+                </div>
+
+                <div>
+                    <img class="videoimg" src="@/assets/images/landing/isakov.png">
+                    <h4>Porta proin tellus sed pulvinar odio eu.</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+                    <div class="info-item"><span class="adressactor">Samatha Leuthex</span>
+                        <span class="infodata">May 5, 3 min read</span>
+                    </div>
+                    <div class="infodown">
+                        <img src="@/assets/images/landing/savesimvol.png">
+                        <img src="@/assets/images/landing/sharesimvol.png">
+                    </div>
+                </div>
+            </div>
+            <div class="popular_questions">
+                See All Popular Questions
+            </div>
+            <div class="break"></div>
+            <div class="allOA"> ALL Q&A</div>
+
+            <div class="ALL">
+                <div class="infovideo">
+                    <video controls>
+                        <source src="https://www.youtube.com/watch?v=Jb1_hbjD8tQ" type="video/mp4">
+                        <source src="https://www.youtube.com/watch?v=Jb1_hbjD8tQ" type="video/ogg">
+                        Your browser does not support the video tag.
+                    </video>
+                </div>
+                <div class="infotext"><span>Massa eu in laoreet enim augue vulputate</span>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        Lectus quisque magna in nisl nibh bibendum ipsum, elit duis. Donec.
+                    </p>
+                    <div class="info-item">
+                        <span class="adressactor">Samatha Leuthex</span>
+                        <span class="infodata">May 5, 3 min read</span>
+                    </div>
+                    <div class="infodown">
+                        <img src="@/assets/images/landing/savesimvol.png">
+                        <img src="@/assets/images/landing/sharesimvol.png">
+                    </div>
+                </div>
+            </div>
+            <div class="ALL">
+                <div class="infovideo">
+                    <video controls>
+                        <source src="https://www.youtube.com/watch?v=Jb1_hbjD8tQ" type="video/mp4">
+                        <source src="https://www.youtube.com/watch?v=Jb1_hbjD8tQ" type="video/ogg">
+                        Your browser does not support the video tag.
+                    </video>
+                </div>
+                <div class="infotext">
+                    <span>
+                      Massa eu in laoreet enim augue vulputate
+                    </span>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        Lectus quisque magna in nisl nibh bibendum ipsum, elit duis. Donec.
+                    </p>
+                    <div class="info-item">
+                    <span class="adressactor">
+                      Samatha Leuthex
+                      </span>
+                                <span class="infodata">
+                      May 5, 3 min read
+                    </span>
+                    </div>
+                    <div class="infodown">
+                        <img src="@/assets/images/landing/savesimvol.png">
+                        <img src="@/assets/images/landing/sharesimvol.png">
+                    </div>
+                </div>
+            </div>
+            <div class="ALL">
+                <div class="infovideo">
+                    <video controls>
+                        <source src="https://www.youtube.com/watch?v=Jb1_hbjD8tQ" type="video/mp4">
+                        <source src="https://www.youtube.com/watch?v=Jb1_hbjD8tQ" type="video/ogg">
+                        Your browser does not support the video tag.
+                    </video>
+                </div>
+                <div class="infotext">
+                    <span>
+                      Massa eu in laoreet enim augue vulputate
+                    </span>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        Lectus quisque magna in nisl nibh bibendum ipsum, elit duis. Donec.
+                    </p>
+                    <div class="info-item">
+                        <span class="adressactor">Samatha Leuthex</span>
+                        <span class="infodata">
+                          May 5, 3 min read
+                        </span>
+                    </div>
+                    <div class="infodown">
+                        <img src="@/assets/images/landing/savesimvol.png">
+                        <img src="@/assets/images/landing/sharesimvol.png">
+                    </div>
+                </div>
+            </div>
+            <div class="postNextButton">
+                <div>
+                    <button class="newPostButton"><img src="@/assets/images/landing/leftNext.png">Назад</button>
+                </div>
+                <div>
+                    <button class="oldPostButton">Вперед <img src="@/assets/images/landing/rightNext.png"></button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Navbar from "@/views/landing/Navbar";
+
+    export default {
+        name: "support",
+        components: {
+            Navbar
+        },
+        data() {
+            return {
+                icon: '\u61442'
+            }
+        },
+        created() {
+            window.addEventListener("resize", this.ChangeSearchInputPlaceholder);
+        },
+        mounted: function mounted() {
+            this.ChangeSearchInputPlaceholder()
+        },
+        beforeDestroy() {
+            window.removeEventListener("resize", this.ChangeSearchInputPlaceholder);
+        },
+        methods: {
+            ChangeSearchInputPlaceholder() {
+                if (window.innerWidth < 400) {
+                    this.$refs.SearchInput.placeholder = "Search";
+                }
+                else {
+                    this.$refs.SearchInput.placeholder = "Search answers related your questions";
+                }
+                console.log(window.innerWidth)
+            },
+        }
+    }
+</script>
+
+<style scoped>
+    .content {
+        width: 100%;
+        box-sizing: border-box;
+        padding: 30px 10% 0 10%;
+        margin-bottom: 200px;
+    }
+
+    .search input[type="text"]::placeholder {
+        /* Firefox, Chrome, Opera */
+        font-style: normal;
+        font-weight: normal;
+        font-size: 16px;
+        line-height: 22px;
+        color: #1F3F68;
+        opacity: 0.6;
+    }
+
+    .search input {
+        width: 95%;
+        height: 56px;
+        background: #F4F4F4;
+        border-radius: 10px;
+        font-size: 16px;
+        border: none;
+        outline: none;
+        text-indent: 60px;
+        color: #1F3F68;
+        opacity: 0.8;
+    }
+
+    .search .form-control-feedback {
+        position: absolute;
+        z-index: 1;
+        padding: 10px 20px 10px 20px;
+        display: block;
+        line-height: 2.375rem;
+        pointer-events: none;
+        color: #1F3F68;
+        opacity: 0.6;
+    }
+
+    .popular {
+        display: block;
+        margin-top: 56px;
+        margin-bottom: 30px;
+        position: relative;
+        width: 98px;
+        height: 27px;
+        font-style: normal;
+        font-weight: 800;
+        font-size: 20px;
+        line-height: 27px;
+        letter-spacing: 0.04em;
+        color: #2F80ED;
+        opacity: 0.8;
+        text-transform: uppercase;
+    }
+
+    h4 {
+        font-style: normal;
+        font-weight: 500;
+        font-size: 28px;
+        line-height: 38px;
+        letter-spacing: 0.04em;
+        color: #1F3F68;
+    }
+
+    p {
+        font-style: normal;
+        font-weight: normal;
+        font-size: 16px;
+        line-height: 26px;
+        /* or 162% */
+
+        letter-spacing: 0.04em;
+
+        color: rgba(0, 0, 0, 0.6);
+    }
+
+    .row {
+        display: flex;
+        justify-content: space-between;
+    }
+
+    .col-5 {
+        width: 40%;
+    }
+
+    .col-6 {
+        height: 379px;
+        float: right;
+        top: 391px;
+        opacity: 0.8;
+        width: 55%;
+    }
+
+    .col-6 img {
+        height: 100%;
+        width: 100%;
+    }
+
+    .PopularsVideoDisplayNone {
+        display: none;
+    }
+
+    h3 {
+        font-style: normal;
+        font-weight: 500;
+        font-size: 48px;
+        line-height: 58px;
+        /* or 121% */
+
+        letter-spacing: 0.04em;
+
+        /* Blue_01 */
+
+        color: #1F3F68;
+    }
+
+    .flexbudet {
+        width: 100%;
+        display: flex;
+        justify-content: space-between;
+        margin-bottom: 54px;
+    }
+
+    .flexbudet div {
+        /*flex: 1;*/
+        width: 30%;
+        margin: 40px 0 0 0;
+
+    }
+
+    .flexbudet p {
+        font-style: normal;
+        font-weight: normal;
+        font-size: 16px;
+        line-height: 26px;
+        /* or 162% */
+
+        letter-spacing: 0.04em;
+
+        color: rgba(0, 0, 0, 0.6);
+    }
+
+    .flexbudet .videoimg {
+        width: 100%;
+    }
+
+    .adressactor {
+        display: block;
+        font-style: normal;
+        font-weight: normal;
+        width: 200px;
+        font-size: 14px;
+        line-height: 24px;
+        /* identical to box height, or 171% */
+        margin-bottom: 0;
+        padding-bottom: 0;
+        letter-spacing: 0.04em;
+
+        /* Blue_01 */
+
+        color: #1F3F68;
+    }
+
+    .infodata {
+        font-style: normal;
+        font-weight: normal;
+        font-size: 14px;
+        line-height: 24px;
+        /* identical to box height, or 171% */
+        position: absolute;
+        letter-spacing: 0.04em;
+        width: 200px;
+
+        color: rgba(0, 0, 0, 0.4);
+    }
+
+    .info-item {
+        position: absolute;
+        margin-top: 50px;
+        float: left;
+        text-align: left;
+        width: 60%;
+        height: 50px;
+        display: inline-block;
+    }
+
+    .infodown {
+        text-align: left;
+        float: right;
+        display: inline-block;
+        width: 40%;
+        margin-top: 50px;
+    }
+
+    .popularinfodown {
+        text-align: left;
+        float: right;
+        display: inline-block;
+        width: 40%;
+        margin-top: 50px;
+    }
+
+    .popularinfodown img {
+        padding: 10px 0 10px 10px;
+        cursor: pointer;
+    }
+
+    .infodown img {
+        cursor: pointer;
+        padding: 10px 0px 5px 20px;
+    }
+
+    .popular_questions {
+
+        font-style: normal;
+        font-weight: 500;
+        font-size: 16px;
+        line-height: 22px;
+        /* identical to box height */
+
+        text-align: right;
+        letter-spacing: 0.04em;
+        text-transform: uppercase;
+        /*background: #F2F2F2;*/
+        margin-top: 50px;
+        color: #2F80ED;
+        opacity: 0.8;
+        margin-bottom: 22px;
+    }
+
+    .break {
+        height: 0;
+        W: 940px;
+        margin-bottom: 56px;
+        border: 1px solid rgba(0, 0, 0, 0.2);
+    }
+
+    .allOA {
+        font-style: normal;
+        font-weight: normal;
+        font-size: 20px;
+        line-height: 27px;
+        letter-spacing: 0.04em;
+        color: #2F80ED;
+        opacity: 0.8;
+
+    }
+
+    .ALL {
+        width: 100%;
+        height: auto;
+        display: flex;
+        justify-content: space-between;
+        margin-top: 30px;
+    }
+
+    .infovideo {
+        height: auto;
+        display: inline-block;
+        width: 30%;
+    }
+
+    .infotext {
+        box-sizing: border-box;
+        margin: 0 20px 0 20px;
+        display: inline;
+        width: 70%;
+        font-style: normal;
+        font-weight: 500;
+        font-size: 28px;
+        line-height: 38px;
+        letter-spacing: 0.04em;
+        color: #1F3F68;
+    }
+
+    .infotext p {
+        font-style: normal;
+        font-weight: normal;
+        font-size: 16px;
+        line-height: 26px;
+        /* or 162% */
+
+        letter-spacing: 0.04em;
+
+        color: rgba(0, 0, 0, 0.6);
+    }
+
+    .postNextButton button {
+        background: none;
+        outline: none;
+        border: none;
+        margin-top: 56px;
+        height: 20px;
+        display: inline-block;
+        font-size: 16px;
+        line-height: 20px;
+        letter-spacing: 0.04em;
+
+        color: #2F80ED;
+    }
+
+    .postNextButton .oldPostButton {
+        display: inline-block;
+        float: right;
+    }
+
+    .postNextButton .newPostButton {
+        display: inline-block;
+        float: left;
+    }
+
+    @media (max-width: 992px) {
+        .infovideo {
+            width: 50%;
+            margin-top: 20px;
+        }
+
+        .infotext {
+            width: 50%;
+            margin-top: 0px;
+        }
+    }
+
+    @media (max-width: 700px) {
+        .search input[type="text"]::placeholder {
+            font-size: 12px;
+        }
+
+        .row {
+            display: block;
+            justify-content: space-between;
+        }
+
+        .col-5 {
+            width: 100%;
+            padding-bottom: 40px;
+        }
+
+        .col-6 {
+            display: block;
+            height: auto;
+            top: 391px;
+            opacity: 0.8;
+            width: 100%;
+        }
+
+        .col-6 img {
+            display: none;
+            height: 100%;
+            width: 100%;
+        }
+
+        .PopularsVideoDisplayNone {
+            display: block;
+        }
+
+        .popularinfodown {
+            text-align: right;
+            float: right;
+            width: 40%;
+
+            margin-top: 10px;
+        }
+
+        .info-item {
+            float: left;
+            text-align: left;
+            width: 60%;
+            height: 50px;
+            margin-top: 10px;
+            display: inline-block;
+
+        }
+
+        .flexbudet {
+            margin-top: 50px;
+            width: 100%;
+            display: block;
+            justify-content: space-between;
+        }
+
+        .flexbudet div {
+            width: auto;
+            padding: 0 0 40px 0;
+        }
+
+        .infodown {
+            margin-top: 10px;
+            float: right;
+            text-align: right;
+        }
+
+        .ALL {
+            width: 100%;
+            display: block;
+            justify-content: space-between;
+            margin-top: 30px;
+            height: auto;
+            padding-bottom: 50px;
+        }
+
+        .infovideo {
+            height: 100%;
+            display: block;
+            width: 100%;
+
+        }
+
+        .infotext {
+            margin-top: 30px;
+        }
+
+        .popular_questions {
+            margin-top: 130px;
+        }
+    }
+</style>
