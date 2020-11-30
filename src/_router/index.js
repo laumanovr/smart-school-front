e8eb3cfb668e58e2e479b25cac24a663b72244ee
Ramazan <laumanov.r@gmail.com>
2020-11-30 18:27:06 +0600

    changed require-auth method in routes

--- src/_router/index.js
+++ src/_router/index.js
@@ -22,54 +22,84 @@ const routes = [
 	{
 		path: '/login',
 		name: 'login',
-		component: Login
+		component: Login,
+        meta: {
+            requireAuth: false
+        }
 	},
     {
         path: '/oauth2/login/esia',
         name: 'loginEsia',
-        component: loadComponent('login/LoginEsi')
+        component: loadComponent('login/LoginEsi'),
+        meta: {
+            requireAuth: false
+        }
     },
 
     // LANDING
     {
         path: '/',
         name: 'home',
-        component: loadComponent('landing/MainPage')
+        component: loadComponent('landing/MainPage'),
+        meta: {
+            requireAuth: false
+        }
     },
     {
         path: '/about',
         name: 'about',
-        component: loadComponent('landing/AboutProject')
+        component: loadComponent('landing/AboutProject'),
+        meta: {
+            requireAuth: false
+        }
     },
     {
         path: '/support',
         name: 'support',
-        component: loadComponent('landing/Support')
+        component: loadComponent('landing/Support'),
+        meta: {
+            requireAuth: false
+        }
     },
     {
         path: '/opportunity',
         name: 'opportunity',
-        component: loadComponent('landing/Opportunities')
+        component: loadComponent('landing/Opportunities'),
+        meta: {
+            requireAuth: false
+        }
     },
     {
         path: '/teacher-opportunity',
         name: 'teacherOpportunity',
-        component: loadComponent('landing/TeacherOpportunity')
+        component: loadComponent('landing/TeacherOpportunity'),
+        meta: {
+            requireAuth: false
+        }
     },
     {
         path: '/gos-opportunity',
         name: 'gosOpportunity',
-        component: loadComponent('landing/GosOpportunity')
+        component: loadComponent('landing/GosOpportunity'),
+        meta: {
+            requireAuth: false
+        }
     },
     {
         path: '/parent-opportunity',
         name: 'parentOpportunity',
-        component: loadComponent('landing/ParentOpportunity')
+        component: loadComponent('landing/ParentOpportunity'),
+        meta: {
+            requireAuth: false
+        }
     },
     {
         path: '/student-opportunity',
         name: 'studentOpportunity',
-        component: loadComponent('landing/StudentOpportunity')
+        component: loadComponent('landing/StudentOpportunity'),
+        meta: {
+            requireAuth: false
+        }
     },
 
     // SUPER-ADMIN
@@ -83,67 +113,106 @@ const routes = [
 			{
 				path: '/',
 				name: 'superAdminDashboard',
-				component: loadComponent('super-admin/SuperAdminDashboard')
+				component: loadComponent('super-admin/SuperAdminDashboard'),
+                meta: {
+                    requireAuth: true
+                }
 			},
 			{
 				name: 'superAdminSchools',
 				path: 'schools',
-				component: loadComponent('super-admin/Schools')
+				component: loadComponent('super-admin/Schools'),
+                meta: {
+                    requireAuth: true
+                }
 			},
 			{
 				name: 'superSchoolAdmin',
 				path: 'school-admin',
-				component: SchoolAdmin
+				component: SchoolAdmin,
+                meta: {
+                    requireAuth: true
+                }
 			},
             {
                 name: 'rayonHead',
                 path: 'rayon-head',
-                component: loadComponent('super-admin/RayonHead')
+                component: loadComponent('super-admin/RayonHead'),
+                meta: {
+                    requireAuth: true
+                }
             },
 			{
 				name: 'superAdminInstructors',
 				path: 'instructors',
-				component: Instructor
+				component: Instructor,
+                meta: {
+                    requireAuth: true
+                }
 			},
             {
                 name: 'superAdminStudents',
                 path: 'students',
-                component: Students
+                component: Students,
+                meta: {
+                    requireAuth: true
+                }
             },
 			{
 				name: 'superAdminCourses',
 				path: 'courses',
-				component: Courses
+				component: Courses,
+                meta: {
+                    requireAuth: true
+                }
 			},
 			{
 				name: 'superAdminRayon',
 				path: 'rayon',
-				component: Rayon
+				component: Rayon,
+                meta: {
+                    requireAuth: true
+                }
 			},
 			{
 				name: 'superAdminCalendar',
 				path: 'calendar',
-				component: Calendar
+				component: Calendar,
+                meta: {
+                    requireAuth: true
+                }
 			},
 			{
 				name: 'superAdminAnnouncement',
 				path: 'announcements',
-				component: loadComponent('super-admin/Announcements')
+				component: loadComponent('super-admin/Announcements'),
+                meta: {
+                    requireAuth: true
+                }
 			},
 			{
 				name: 'superAdminLibrary',
 				path: 'library',
-				component: loadComponent('super-admin/Library')
+				component: loadComponent('super-admin/Library'),
+                meta: {
+                    requireAuth: true
+                }
 			},
             {
                 name: 'superAdminChronicle',
                 path: 'chronicle',
-                component: Chronicle
+                component: Chronicle,
+                meta: {
+                    requireAuth: true
+                }
             },
             {
                 name: 'superAdminGradeReason',
                 path: 'grade-reason',
-                component: loadComponent('super-admin/AdminGradeReason')
+                component: loadComponent('super-admin/AdminGradeReason'),
+                meta: {
+                    requireAuth: true
+                }
             }
 		],
 	},
@@ -159,62 +228,98 @@ const routes = [
 			{
 				name: 'schoolAdminDashboard',
 				path: '/',
-				component: Dashboard
+				component: Dashboard,
+                meta: {
+                    requireAuth: true
+                }
 			},
 			{
 				name: 'schoolAdminTeachers',
 				path: 'teachers',
-				component: Teachers
+				component: Teachers,
+                meta: {
+                    requireAuth: true
+                }
 			},
 			{
 				name: 'schoolAdminCourses',
 				path: 'courses',
-				component: SchoolAdminCourses
+				component: SchoolAdminCourses,
+                meta: {
+                    requireAuth: true
+                }
 			},
 			{
 				name: 'schoolAdminClasses',
 				path: 'classes',
-				component: loadComponent('school-admin/SchoolAdminClasses')
+				component: loadComponent('school-admin/SchoolAdminClasses'),
+                meta: {
+                    requireAuth: true
+                }
 			},
 			{
 				name: 'schoolAdminStudents',
 				path: 'students',
-				component: loadComponent('school-admin/Students')
+				component: loadComponent('school-admin/Students'),
+                meta: {
+                    requireAuth: true
+                }
 			},
 			{
 				name: 'schoolAdminAnnouncements',
 				path: 'announcements',
-				component: loadComponent('school-admin/Announcements')
+				component: loadComponent('school-admin/Announcements'),
+                meta: {
+                    requireAuth: true
+                }
 			},
 			{
 				name: 'library',
 				path: 'library',
-				component: loadComponent('school-admin/Library')
+				component: loadComponent('school-admin/Library'),
+                meta: {
+                    requireAuth: true
+                }
 			},
 			{
 				name: 'schoolAdminScheduleWeek',
 				path: 'schedule-week',
-				component: loadComponent('school-admin/SchoolAdminScheduleWeek')
+				component: loadComponent('school-admin/SchoolAdminScheduleWeek'),
+                meta: {
+                    requireAuth: true
+                }
 			},
 			{
 				name: 'aboutSchool',
 				path: 'about-school',
-				component: loadComponent('school-admin/AboutSchool')
+				component: loadComponent('school-admin/AboutSchool'),
+                meta: {
+                    requireAuth: true
+                }
 			},
 			{
 				name: 'schoolAdminQuarter',
 				path: 'school-quarter',
-				component: loadComponent('school-admin/QuarterCalendar')
+				component: loadComponent('school-admin/QuarterCalendar'),
+                meta: {
+                    requireAuth: true
+                }
 			},
 			{
 				name: 'shifts',
 				path: 'shifts',
-				component: loadComponent('school-admin/Shift')
+				component: loadComponent('school-admin/Shift'),
+                meta: {
+                    requireAuth: true
+                }
 			},
             {
                 name: 'adminReport',
                 path: 'admin-report',
-                component: loadComponent('school-admin/SchoolAdminReport')
+                component: loadComponent('school-admin/SchoolAdminReport'),
+                meta: {
+                    requireAuth: true
+                }
             }
 		],
 	},
@@ -231,46 +336,74 @@ const routes = [
                 name: 'instructorDashboard',
                 path: '/',
                 component: loadComponent('instructor/InstructorDashboard'),
+                meta: {
+                    requireAuth: true
+                }
             },
 	        {
 		        name: 'instructorQuarterGrade',
 		        path: 'quarter-grade',
 		        component: loadComponent('instructor/InstructorQuarterGrade'),
+                meta: {
+                    requireAuth: true
+                }
 	        },
 	        {
 		        name: 'instructorTopics',
 		        path: 'topics',
 		        component: loadComponent('instructor/InstructorTopic'),
+                meta: {
+                    requireAuth: true
+                }
 	        },
             {
                 name: 'instructorClasses',
                 path: 'classes',
-                component: loadComponent('instructor/InstructorClasses')
+                component: loadComponent('instructor/InstructorClasses'),
+                meta: {
+                    requireAuth: true
+                }
             },
             {
                 name: 'instructorSchedule',
                 path: 'schedule',
-                component: loadComponent('instructor/InstructorSchedule')
+                component: loadComponent('instructor/InstructorSchedule'),
+                meta: {
+                    requireAuth: true
+                }
             },
             {
                 name: 'instructorJournalGrade',
                 path: 'journal-grade',
-                component: loadComponent('instructor/InstructorJournalGrade')
+                component: loadComponent('instructor/InstructorJournalGrade'),
+                meta: {
+                    requireAuth: true
+                }
             },
             {
                 name: 'instructorOnlineLesson',
                 path: 'online-lesson',
-                component: loadComponent('instructor/InstructorOnlineLesson')
+                component: loadComponent('instructor/InstructorOnlineLesson'),
+                meta: {
+                    requireAuth: true
+                }
             },
             {
                 name: 'instructorReport',
                 path: 'instructor-report',
-                component: loadComponent('instructor/InstructorReport')
+                component: loadComponent('instructor/InstructorReport'),
+                meta: {
+                    requireAuth: true
+                }
             },
             {
                 name: 'instructorMessage',
                 path: 'message',
-                component: SuperAdminEmpty // Временно
+                component: SuperAdminEmpty,
+                meta: {
+                    requireAuth: true
+                }
+                // Временно
             },
         ],
     },
@@ -286,27 +419,42 @@ const routes = [
             {
                 path: '/',
                 name: 'rayonHeadDashboard',
-                component: loadComponent('rayon-head/Dashboard')
+                component: loadComponent('rayon-head/Dashboard'),
+                meta: {
+                    requireAuth: true
+                },
             },
             {
                 name: 'rayonHeadSchools',
                 path: 'schools',
-                component: loadComponent('rayon-head/Schools')
+                component: loadComponent('rayon-head/Schools'),
+                meta: {
+                    requireAuth: true
+                },
             },
             {
                 name: 'rayonHeadInstructors',
                 path: 'instructors',
-                component: loadComponent('rayon-head/Instructors')
+                component: loadComponent('rayon-head/Instructors'),
+                meta: {
+                    requireAuth: true
+                },
             },
             {
                 name: 'rayonHeadStudents',
                 path: 'students',
-                component: loadComponent('rayon-head/Students')
+                component: loadComponent('rayon-head/Students'),
+                meta: {
+                    requireAuth: true
+                },
             },
             {
                 name: 'rayonHeadReport',
                 path: 'rayon-head-report',
-                component: loadComponent('rayon-head/RayonHeadReport')
+                component: loadComponent('rayon-head/RayonHeadReport'),
+                meta: {
+                    requireAuth: true
+                },
             },
             // {
             //     name: 'rayonHeadCalendar',
@@ -322,16 +470,13 @@ const router = new VueRouter({
 	mode: 'history',
 	base: process.env.BASE_URL,
 	routes
-})
-router.beforeEach((to, from, next) => {
-	const publicPages = [ '/login', '/', '/oauth2/login/esia' ];
-	const authRequired = !publicPages.includes(to.path);
-	const loggedIn = localStorage.getItem('user');
+});
 
-	if (authRequired && !loggedIn) {
+router.beforeEach((to, from, next) => {
+	const loggedIn = localStorage.getItem('user');
+	if (to.meta.requireAuth && !loggedIn) {
 		return next('/login');
 	}
-
 	next();
 });
 
