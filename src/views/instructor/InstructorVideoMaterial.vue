8186ec72ceda62a38040de42b46d7c83409762c8
Ramazan <laumanov.r@gmail.com>
2020-12-03 11:55:37 +0600

    create video-material component for instr

---
+++ src/views/instructor/InstructorVideoMaterial.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="instructor-video-material">
+        <VideoInstruction
+            :headTitle="'Видео инструкции по Умной Школе для учителя'"
+            :role="'ROLE_INSTRUCTOR'"
+        />
+    </div>
+</template>
+
+<script>
+    import VideoInstruction from '@/components/video-instruction/VideoInstruction';
+    export default {
+        components: {
+            VideoInstruction
+        }
+    }
+</script>
