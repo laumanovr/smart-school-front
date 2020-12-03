0b04d22585ad75371cc32ff58336c06e148f9511
Ramazan <laumanov.r@gmail.com>
2020-12-03 11:54:52 +0600

    create video-material component for school-admin

---
+++ src/views/school-admin/SchoolAdminVideoMaterial.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="school-admin-video-material">
+        <VideoInstruction
+            :headTitle="'Видео инструкции по Умной Школе для админа'"
+            :role="'ROLE_ADMIN'"
+        />
+    </div>
+</template>
+
+<script>
+    import VideoInstruction from '@/components/video-instruction/VideoInstruction';
+    export default {
+        components: {
+            VideoInstruction
+        }
+    }
+</script>
