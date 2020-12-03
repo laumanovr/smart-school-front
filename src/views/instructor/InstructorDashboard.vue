88e9addd1dc41b4d228a4c6fe04050e32066ebec
Ramazan <laumanov.r@gmail.com>
2020-12-03 14:49:04 +0600

    add redirect to grade in instr

--- src/views/instructor/InstructorDashboard.vue
+++ src/views/instructor/InstructorDashboard.vue
@@ -1,22 +1,25 @@
 <template>
 	<div class="instructor-dashboard">
-		<div class="instructor-dashboard__schedule">
-			<img src="../../assets/images/temporary/instructor-schedule.svg">
-		</div>
-		<div class="instructor-dashboard__stat stat">
-			<div class="stat__my-class">
-				<img alt="" src="../../assets/images/temporary/my-class-chart.svg">
-			</div>
-			<div class="stat__students">
-				<img src="../../assets/images/temporary/student-stats.svg" alt="">
-			</div>
-		</div>
+		<!--<div class="instructor-dashboard__schedule">-->
+			<!--<img src="../../assets/images/temporary/instructor-schedule.svg">-->
+		<!--</div>-->
+		<!--<div class="instructor-dashboard__stat stat">-->
+			<!--<div class="stat__my-class">-->
+				<!--<img alt="" src="../../assets/images/temporary/my-class-chart.svg">-->
+			<!--</div>-->
+			<!--<div class="stat__students">-->
+				<!--<img src="../../assets/images/temporary/student-stats.svg" alt="">-->
+			<!--</div>-->
+		<!--</div>-->
 	</div>
 </template>
 
 <script>
 export default {
-	name: "InstructorDashboard"
+	name: "InstructorDashboard",
+    beforeCreate() {
+	    this.$router.push('/instructor/journal-grade');
+    }
 }
 </script>
 
