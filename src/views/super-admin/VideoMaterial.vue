9a8016d9007f0074a70f61dcfcff50fa19455286
Ramazan <laumanov.r@gmail.com>
2020-12-03 11:54:24 +0600

    create video-material component for super-admin

---
+++ src/views/super-admin/VideoMaterial.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="super-admin-video-material">
+        <VideoInstruction
+            :headTitle="'Видео материалы'"
+            :role="'superAdmin'"
+        />
+    </div>
+</template>
+
+<script>
+    import VideoInstruction from '@/components/video-instruction/VideoInstruction';
+    export default {
+        components: {
+            VideoInstruction
+        }
+    }
+</script>
