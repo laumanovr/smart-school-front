5e848f9483f8b8e3312f46e25668caed675bc08d
Ramazan <laumanov.r@gmail.com>
2020-12-05 13:39:26 +0600

    change add api to add attachment for assignment

--- src/_services/assignment.service.js
+++ src/_services/assignment.service.js
@@ -20,28 +20,43 @@ export  class AssignmentService extends BaseApiService {
     return this.sendDeleteRequest(url);
   }
 
-  create (data) {
-    const url = `${process.env.VUE_APP_BASE_URL}/v1/assignment/create`;
-    return this.sendPostRequest(url, data);
+  create(data) {
+      const url = `${process.env.VUE_APP_BASE_URL}/v1/assignment/create`;
+      const formData = new FormData();
+      for (let key in data) {
+          formData.append(key, data[key]);
+      }
+      return this.sendPostRequest(url, formData);
   }
 
-  edit (data) {
-    const url = `${process.env.VUE_APP_BASE_URL}/v1/assignment/edit/${data.id}`;
-    return this.sendPostRequest(url, data);
+  attachFile(assignmentId, file) {
+      const url = `${process.env.VUE_APP_BASE_URL}/v1/assignment/add-file/${assignmentId}`;
+      const formData = new FormData();
+      formData.append('files', file);
+      return this.sendPostRequest(url, formData);
+  }
+
+  edit(data) {
+      const url = `${process.env.VUE_APP_BASE_URL}/v1/assignment/edit/${data.id}`;
+      const formData = new FormData();
+      for (let key in data) {
+          formData.append(key, data[key]);
+      }
+      return this.sendPostRequest(url, formData);
   }
 
   getByInstructor (id) {
-    const url = `${process.env.VUE_APP_BASE_URL}/v1/assignment/list/instructor/${id}`
+    const url = `${process.env.VUE_APP_BASE_URL}/v1/assignment/list/instructor/${id}`;
     return this.sendGetRequest(url)
   }
 
   getByInstructorCourse (instructorId, courseId) {
-    const url = `${process.env.VUE_APP_BASE_URL}/v1/assignment/list/instructor/${instructorId}/course/${courseId}`
+    const url = `${process.env.VUE_APP_BASE_URL}/v1/assignment/list/instructor/${instructorId}/course/${courseId}`;
     return this.sendGetRequest(url)
   }
 
   getByTopic (id) {
-    const url = `${process.env.VUE_APP_BASE_URL}/v1/assignment/list/topic/${id}`
+    const url = `${process.env.VUE_APP_BASE_URL}/v1/assignment/list/topic/${id}`;
     return this.sendGetRequest(url)
   }
 }
