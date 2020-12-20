372cda4daf6333e72c30889accb47c89bc526745
Ramazan <laumanov.r@gmail.com>
2020-12-20 11:07:04 +0600

    create setting-manage-page

---
+++ src/views/settings/SettingsManage.vue
@@ -0,0 +1,105 @@
+<template>
+    <div class="settings-manage">
+        <SuperAdminHeader>
+            <template v-slot:title>~</template>
+            <template v-slot:userName v-if="userProfile.user">
+                {{ userProfile.user.firstName}} {{ userProfile.user.lastName }}
+            </template>
+            <template v-slot:roleName>Настройки</template>
+        </SuperAdminHeader>
+        <div class="settings-manage__container">
+            <Sidebar :role="roleObj[userProfile.role[0].code]"/>
+            <div class="settings-manage__body">
+                <div class="settings-manage__main">
+                    <div class="header-title">
+                        <h3>Настройки</h3>
+                    </div>
+                    <div class="menu-selections">
+                        <div class="selection" :class="{'active': $route.name == 'changePassword'}">Смена пароля</div>
+                        <div class="selection">Смена ФИО</div>
+                    </div>
+                    <div class="menu-content">
+                        <router-view></router-view>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import SuperAdminHeader from '@/components/navigations/sidebars/SuperAdminHeader';
+    import Sidebar from '@/components/navigations/sidebars/Sidebar';
+
+    export default {
+        components: {Sidebar, SuperAdminHeader},
+        computed: {
+            userProfile() {
+                return this.$store.state.account.profile;
+            }
+        },
+        data() {
+            return {
+                roleObj: {
+                    'ROLE_SUPER_ADMIN': 'superAdmin',
+                    'ROLE_ADMIN': 'schoolAdmin',
+                    'ROLE_INSTRUCTOR': 'instructor',
+                    'ROLE_RAYON_HEADER': 'rayonHead'
+                }
+            }
+        },
+    }
+</script>
+
+<style lang="scss">
+    .settings-manage {
+        margin-bottom: 50px;
+        background: #f8f8fb;
+        &__container {
+            display: flex;
+        }
+        &__body {
+            width: calc(100% - 100px);
+            margin-left: 100px;
+            min-height: 88vh;
+        }
+        &__main {
+            margin: 0 30px;
+        }
+        .super-admin-sidebar {
+            position: fixed;
+            top: 0;
+            height: 100vh;
+        }
+        .header-title {
+            text-align: center;
+            h3 {
+
+            }
+        }
+        .menu-selections {
+            display: flex;
+            align-items: center;
+            .selection {
+                margin-right: 15px;
+                border: 1px solid #dadada;
+                background: #fff;
+                padding: 2px 8px;
+                border-radius: 4px;
+                cursor: pointer;
+                &.active {
+                    background: #1976d2;
+                    color: #fff;
+                    padding: 2px 8px;
+                    border-radius: 4px;
+                    cursor: default;
+                }
+            }
+        }
+        .menu-content {
+            border-top: 1px solid #d4d4d4;
+            margin-top: 20px;
+            padding-top: 25px;
+        }
+    }
+</style>
