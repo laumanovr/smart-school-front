8bd80506bf97474af6e6db44341da20ba17643bb
Ramazan <laumanov.r@gmail.com>
2020-12-21 16:18:12 +0600

    add title-kg to grade-reason super-admin

--- src/views/super-admin/AdminGradeReason.vue
+++ src/views/super-admin/AdminGradeReason.vue
@@ -8,24 +8,20 @@
             <v-btn color="primary" @click="openGradeReasonModal('create', {})">Добавить</v-btn>
         </div>
 
-        <table class="table bordered reason">
-            <thead>
-                <tr>
-                    <th class="light-purple">№</th>
-                    <th>Предмет</th>
-                    <th>Название</th>
-                    <th class="blue-light"></th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="(reason, i) in allGradeReasons" :key="reason.id">
-                    <td class="light-purple">{{ i + 1 }}</td>
-                    <td>{{ reason.adminCourse.title }}</td>
+        <div class="reason-tables" v-for="(item, i) in allGradeReasons" :key="item.id">
+            <div class="course-title"><span>{{ item.courseTitle }}</span></div>
+            <table class="table bordered reason">
+                <tbody>
+                <tr v-for="reason in item.reasons">
                     <td>{{ reason.title }}</td>
-                    <td class="blue-light"><EditIcon @click="openGradeReasonModal('update', reason)"/></td>
+                    <td>{{ reason.titleKG }}</td>
+                    <td class="blue-light">
+                        <EditIcon @click="openGradeReasonModal('update', reason)"/>
+                    </td>
                 </tr>
-            </tbody>
-        </table>
+                </tbody>
+            </table>
+        </div>
 
         <!--MODAL FOR ADD/EDIT GRADE REASON-->
         <modal name="grade-reason-modal" height="auto">
@@ -40,7 +36,8 @@
                         :rules="required"
                         v-model="gradeReasonObj.adminCourseId"
                     />
-                    <v-text-field v-model="gradeReasonObj.title" label="Название" :rules="required"/>
+                    <v-text-field v-model="gradeReasonObj.title" label="На русском" :rules="required"/>
+                    <v-text-field v-model="gradeReasonObj.titleKG" label="На кыргызском" :rules="required"/>
                     <div class="btn-actions">
                         <v-btn color="red" @click="hideReasonModal">Отмена</v-btn>
                         <v-btn color="green" @click="submitGradeReason">Сохранить</v-btn>
@@ -69,6 +66,7 @@
                 gradeReasonObj: {
                     adminCourseId: 0,
                     title: '',
+                    titleKG: '',
                     classLevel: ''
                 },
                 isLoading: false,
@@ -85,9 +83,19 @@
 
         methods: {
             fetchAllAdminGradeReasons() {
+                this.allGradeReasons = [];
                 this.isLoading = true;
                 AdminGradeReasonService.getAll().then((res) => {
-                    this.allGradeReasons = res.sort((a, b) => a.adminCourse.title.localeCompare(b.adminCourse.title));
+                    Object.entries(res.reduce((obj, el) => {
+                        obj[el.adminCourse.id] = [...obj[el.adminCourse.id] || [], el];
+                        return obj;
+                    }, {})).forEach((item, i) => {
+                        const reasonObj = {id: i+1, courseTitle: item[1][0].adminCourse.title, reasons: []};
+                        item[1].forEach((reason) => {
+                            reasonObj.reasons.push(reason);
+                        });
+                        this.allGradeReasons.push(reasonObj);
+                    });
                     this.isLoading = false;
                 }).catch((err) => {
                     this.$toast.error(err);
@@ -105,10 +113,12 @@
                 this.mode = mode;
                 if (mode === 'create') {
                     this.gradeReasonObj.title ='';
+                    this.gradeReasonObj.titleKG = '';
                     this.gradeReasonObj.classLevel = '';
                     this.gradeReasonObj.adminCourseId = '';
                 } else {
                     this.gradeReasonObj.title = reasonObj.title;
+                    this.gradeReasonObj.titleKG = reasonObj.titleKG;
                     this.gradeReasonObj.classLevel = reasonObj.classLevel;
                     this.gradeReasonObj.adminCourseId = reasonObj.adminCourse.id;
                     this.gradeReasonObj.id = reasonObj.id;
@@ -145,9 +155,32 @@
             align-items: center;
             justify-content: space-between;
         }
-        table.reason {
-            max-width: 1060px;
-            margin: 0 auto;
+        .reason-tables {
+            margin-bottom: 25px;
+            .course-title {
+                max-width: 1020px;
+                margin: 0 auto;
+                text-align: center;
+                background: #fff;
+                border: 1px solid #b5b5b5;
+                border-bottom: 0;
+                color: #444444;
+                span {
+                    transform: translateX(-40px);
+                    display: inline-block;
+                }
+            }
+            table.reason {
+                max-width: 1020px;
+                margin: 0 auto;
+                td {
+                    width: 460px;
+                    height: 43px;
+                }
+                .blue-light {
+                    width: 100px;
+                }
+            }
         }
     }
 </style>
