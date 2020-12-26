6a960e0e2825961bde3b69869349ad09616507e7
Ramazan <laumanov.r@gmail.com>
2020-12-26 09:55:10 +0600

    component for handel login with esia

--- src/views/login/LoginEsi.vue
+++ src/views/login/LoginEsi.vue
@@ -1,1 +1,36 @@
-<template><h3>ESIA</h3></template>
+<template>
+    <div class="esia">
+        <PreLoader v-if="isLoading"/>
+        <h4>ESIA</h4>
+        <!--<button @click="fetchUserInfo">get-user</button>-->
+    </div>
+</template>
+
+<script>
+    import PreLoader from '@/components/preloader/PreLoader';
+    import EsiaService from '@/_services/esia.service';
+
+    export default {
+        components: {
+            PreLoader
+        },
+        data() {
+            return {
+                isLoading: false,
+            }
+        },
+        created() {
+            EsiaService.getAccessToken(this.$route.query.code).then((res) => {
+                debugger
+                this.fetchUserInfo(res.access_token)
+            })
+        },
+        methods: {
+            fetchUserInfo(token) {
+                EsiaService.getUserInfo(token).then((res) => {
+                    debugger
+                })
+            }
+        }
+    }
+</script>
