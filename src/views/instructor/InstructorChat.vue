c4762012d4e59cf7100ced89ec6b5c13eb6f539c
Ramazan <laumanov.r@gmail.com>
2020-12-27 19:41:05 +0600

    include chat to instructor

---
+++ src/views/instructor/InstructorChat.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="instructor-chat">
+        <Chat/>
+    </div>
+</template>
+
+<script>
+    import Chat from '@/components/chat/Chat';
+
+    export default {
+        components: {
+            Chat
+        }
+    }
+</script>
