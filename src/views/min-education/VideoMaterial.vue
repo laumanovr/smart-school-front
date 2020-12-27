c9a1a3b845609c79237b7e3fa72d7f2b511d6042
Ramazan <laumanov.r@gmail.com>
2020-12-27 10:43:25 +0600

    create video-material-page for min-education

---
+++ src/views/min-education/VideoMaterial.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="super-admin-video-material">
+        <VideoInstruction
+            :headTitle="'Видео материалы'"
+            :role="'minEducation'"
+        />
+    </div>
+</template>
+
+<script>
+    import VideoInstruction from '@/components/video-instruction/VideoInstruction';
+    export default {
+        components: {
+            VideoInstruction
+        }
+    }
+</script>
